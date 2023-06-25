6baeca63a727c44274fad0e562273932cdd963bc
sieuhuflit <sieuhuflit@gmail.com>
2023-06-25 18:47:42 +0700

    feat(setup): show env variables info

--- lib/presentation/auth/pages/login_page.dart
+++ lib/presentation/auth/pages/login_page.dart
@@ -4,6 +4,9 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 // import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:app/core/config.dart';
+
+
 
 @RoutePage()
 class LoginPage extends StatefulWidget {
@@ -12,19 +15,27 @@ class LoginPage extends StatefulWidget {
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
-
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: ElevatedButton(
-          onPressed: () {
-            context.read<AuthBloc>().add(const AuthEvent.login());
-          },
-          child: const Text(
-            "Login",
-          )),
-    ));
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              AppConfig.env,
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                context.read<AuthBloc>().add(const AuthEvent.login());
+              },
+              child: const Text("Login"),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
