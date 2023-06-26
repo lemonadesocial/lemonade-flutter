56d7e323eb35441e821ac5164ba285cb9724902c
Thinh CP <thinh@coderpush.com>
2023-06-26 09:54:11 +0700

    feat(events-listing): get events-listing - Display events listing data to events-listing-page

---
+++ lib/core/presentation/pages/event/events_listing_page.dart
@@ -0,0 +1,161 @@
+import 'package:app/core/application/auth/auth_bloc.dart';
+import 'package:app/core/application/event/events_listing_bloc/events_listing_bloc.dart';
+import 'package:app/core/domain/event/event_enums.dart';
+import 'package:app/core/domain/event/event_repository.dart';
+import 'package:app/core/service/event/event_service.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/core/presentation/pages/event/widgets/event_card_widget.dart';
+import 'package:app/core/presentation/pages/event/widgets/event_time_filter_button_widget.dart';
+import 'package:app/core/presentation/widgets/lemon_chip_widget.dart';
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/injection/register_module.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+@RoutePage()
+class EventsListingPage extends StatelessWidget {
+  late final EventService eventService = EventService(getIt<EventRepository>());
+
+  Widget _eventsListingBlocProvider(Widget child) {
+    return BlocProvider<EventsListingBloc>(
+      create: (context) => EventsListingBloc(eventService)..add(EventsListingEvent.fetch()),
+      child: child,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _eventsListingBlocProvider(
+      _EventsListingView(),
+    );
+  }
+}
+
+class _EventsListingView extends StatefulWidget {
+  const _EventsListingView();
+
+  @override
+  State<_EventsListingView> createState() => _EventsListingViewState();
+}
+
+class _EventsListingViewState extends State<_EventsListingView> {
+  EventListingType eventListingType = EventListingType.all;
+  EventTimeFilter? eventTimeFilter;
+
+  _selectEventListingType(EventListingType _eventListingType) {
+    setState(() {
+      eventListingType = _eventListingType;
+      context
+          .read<EventsListingBloc>()
+          .add(EventsListingEvent.fetch(eventListingType: _eventListingType, eventTimeFilter: eventTimeFilter));
+    });
+  }
+
+  _selectEventTimeFilter(EventTimeFilter? _eventTimeFilter) {
+    setState(() {
+      eventTimeFilter = _eventTimeFilter;
+    });
+    context.read<EventsListingBloc>().add(EventsListingEvent.filter(eventTimeFilter: eventTimeFilter));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Translations.of(context);
+    final themeColor = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(t.event.events),
+        leading: const Icon(Icons.menu_rounded),
+        actions: [
+          ThemeSvgIcon(color: themeColor.onSurface, builder: (filter) => Assets.icons.icChat.svg(colorFilter: filter)),
+          SizedBox(width: Spacing.medium),
+        ],
+      ),
+      backgroundColor: themeColor.primary,
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: Spacing.small),
+        child: Column(
+          children: [
+            SizedBox(height: Spacing.xSmall),
+            _buildEventsFilterBar(context),
+            SizedBox(height: Spacing.small),
+            // _buildEventsListing(),
+            _buildEventsListing()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildEventsFilterBar(BuildContext ctx) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        LemonChip(
+          label: t.event.all,
+          isActive: eventListingType == EventListingType.all,
+          onTap: () => _selectEventListingType(EventListingType.all),
+        ),
+        BlocBuilder<AuthBloc, AuthState>(builder: (context, authState) {
+          return authState.maybeWhen(
+              authenticated: () => Row(
+                    children: [
+                      SizedBox(width: Spacing.superExtraSmall),
+                      LemonChip(
+                        label: t.event.attending,
+                        isActive: eventListingType == EventListingType.attending,
+                        onTap: () => _selectEventListingType(EventListingType.attending),
+                      ),
+                      SizedBox(width: Spacing.superExtraSmall),
+                      LemonChip(
+                        label: t.event.hosting,
+                        isActive: eventListingType == EventListingType.hosting,
+                        onTap: () => _selectEventListingType(EventListingType.hosting),
+                      ),
+                    ],
+                  ),
+              orElse: () => SizedBox.shrink());
+        }),
+        const Spacer(),
+        EventTimeFilterButton(
+          onSelect: (filter) => _selectEventTimeFilter(filter),
+        ),
+      ],
+    );
+  }
+
+  _buildEventsListing() {
+    return BlocBuilder<EventsListingBloc, EventsListingState>(
+      builder: (context, eventsListingState) {
+        return eventsListingState.when(
+          loading: () {
+            return Expanded(
+                child: Center(
+              child: CircularProgressIndicator(color: Colors.red),
+            ));
+          },
+          fetched: (_, filteredEvents) {
+            return Expanded(
+              child: ListView.separated(
+                itemBuilder: (ctx, index) => index == filteredEvents.length
+                    ? const SizedBox(height: 80)
+                    : EventCard(
+                        key: Key(filteredEvents[index].id ?? ''),
+                        event: filteredEvents[index],
+                      ),
+                separatorBuilder: (ctx, index) => SizedBox(height: Spacing.extraSmall),
+                itemCount: filteredEvents.length + 1,
+              ),
+            );
+          },
+          failure: () => Center(
+            child: Text('error'),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/core/presentation/pages/event/widgets/event_card_widget.dart
@@ -0,0 +1,107 @@
+import 'package:app/core/domain/event/entities/event.dart';
+import 'package:app/core/utils/date_format_utils.dart';
+import 'package:app/core/utils/image_utils.dart';
+import 'package:app/core/utils/number_utils.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/core/presentation/widgets/lemon_circle_avatar_widget.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/typo.dart';
+import 'package:app/theme/sizing.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class EventCard extends StatelessWidget {
+  final Event event;
+  const EventCard({
+    super.key,
+    required this.event,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        border: Border.all(color: colorScheme.outline),
+      ),
+      child: Column(
+        children: [
+          _buildCardHeader(),
+          _buildCardBody(),
+          _buildCardFooter(context, colorScheme),
+        ],
+      ),
+    );
+  }
+
+  _buildCardHeader() => Padding(
+        padding: EdgeInsets.symmetric(horizontal: Spacing.xSmall, vertical: Spacing.xSmall),
+        child: LemonCircleAvatar(
+          url: ImageUtils.generateUrl(
+              file: event.hostExpanded?.newPhotosExpanded?.first, imageConfig: ImageConfig.profile),
+          label: event.hostExpanded?.name ?? '',
+        ),
+      );
+
+  _buildCardBody() => SizedBox(
+        width: double.infinity,
+        // height: 200,
+        child: CachedNetworkImage(
+          width: double.infinity,
+          fit: BoxFit.cover,
+          imageUrl:
+              ImageUtils.generateUrl(file: event.newNewPhotosExpanded?.first, imageConfig: ImageConfig.eventPhoto),
+        ),
+      );
+
+  _buildCardFooter(BuildContext context, ColorScheme colorScheme) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: Spacing.small, vertical: Spacing.small),
+        child: Row(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(event.title ?? ''),
+                SizedBox(height: Spacing.superExtraSmall),
+                Text(DateFormatUtils.fullDateWithTime(event.end),
+                    style: Typo.small.copyWith(color: colorScheme.onSecondary)),
+              ],
+            ),
+            const Spacer(),
+            _buildBuyTicketButton(context, colorScheme)
+          ],
+        ),
+      );
+
+  _buildBuyTicketButton(BuildContext ctx, ColorScheme colorScheme) {
+    return GestureDetector(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: Spacing.xSmall, vertical: Spacing.extraSmall),
+        decoration: BoxDecoration(
+          border: Border.all(color: colorScheme.outline),
+          borderRadius: BorderRadius.circular(LemonRadius.normal),
+          color: LemonColor.lavender18,
+        ),
+        child: Row(
+          children: [
+            Assets.icons.icTicket.svg(
+              width: Sizing.xSmall,
+              height: Sizing.xSmall,
+              colorFilter: ColorFilter.mode(LemonColor.paleViolet, BlendMode.srcIn),
+            ),
+            SizedBox(width: Spacing.superExtraSmall),
+            Text(
+              event.cost != null
+                  ? '${NumberUtils.formatCurrency(amount: event.cost!, currency: event.currency, freeText: t.event.free, prefix: '${t.event.buy} • ')}'
+                  : '${t.event.free}',
+              style: Typo.small.copyWith(color: LemonColor.paleViolet),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
