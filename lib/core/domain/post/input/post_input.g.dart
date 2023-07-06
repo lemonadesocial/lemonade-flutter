fd9a6fe09e0447ab1fc2a8ca8ec71e8a2dd6578e
Thinh CP <thinh@coderpush.com>
2023-07-06 13:26:12 +0700

    feat(profile): define post input

---
+++ lib/core/domain/post/input/post_input.g.dart
@@ -0,0 +1,80 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'post_input.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_GetPostsInput _$$_GetPostsInputFromJson(Map<String, dynamic> json) =>
+    _$_GetPostsInput(
+      input: json['input'] == null
+          ? null
+          : FilterPostsInput.fromJson(json['input'] as Map<String, dynamic>),
+      skip: json['skip'] as int?,
+      limit: json['limit'] as int?,
+    );
+
+Map<String, dynamic> _$$_GetPostsInputToJson(_$_GetPostsInput instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('input', instance.input?.toJson());
+  writeNotNull('skip', instance.skip);
+  writeNotNull('limit', instance.limit);
+  return val;
+}
+
+_$_FilterPostsInput _$$_FilterPostsInputFromJson(Map<String, dynamic> json) =>
+    _$_FilterPostsInput(
+      id: json['_id'] as String?,
+      user: json['user'] as String?,
+      published: json['published'] as bool?,
+      createdAt: json['created_at'] == null
+          ? null
+          : GetPostCreatedAtInput.fromJson(
+              json['created_at'] as Map<String, dynamic>),
+    );
+
+Map<String, dynamic> _$$_FilterPostsInputToJson(_$_FilterPostsInput instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('_id', instance.id);
+  writeNotNull('user', instance.user);
+  writeNotNull('published', instance.published);
+  writeNotNull('created_at', instance.createdAt?.toJson());
+  return val;
+}
+
+_$_GetPostCreatedAtInput _$$_GetPostCreatedAtInputFromJson(
+        Map<String, dynamic> json) =>
+    _$_GetPostCreatedAtInput(
+      gte: json['gte'] == null ? null : DateTime.parse(json['gte'] as String),
+      lte: json['lte'] == null ? null : DateTime.parse(json['lte'] as String),
+    );
+
+Map<String, dynamic> _$$_GetPostCreatedAtInputToJson(
+    _$_GetPostCreatedAtInput instance) {
+  final val = <String, dynamic>{};
+
+  void writeNotNull(String key, dynamic value) {
+    if (value != null) {
+      val[key] = value;
+    }
+  }
+
+  writeNotNull('gte', instance.gte?.toIso8601String());
+  writeNotNull('lte', instance.lte?.toIso8601String());
+  return val;
+}
