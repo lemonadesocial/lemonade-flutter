34048253b8d2aeac74ce149dfa1c83ebfa146f24
Thinh CP <thinh@coderpush.com>
2023-07-11 09:46:15 +0700

    demo: trigger notification bloc

--- lib/core/presentation/pages/notification/notifications_listing_page.dart
+++ lib/core/presentation/pages/notification/notifications_listing_page.dart
@@ -1,7 +1,6 @@
 import 'package:app/core/application/notification/notifications_listing_bloc.dart';
-import 'package:app/core/domain/notification/notification_repository.dart';
+import 'package:app/core/data/notification/notification_repository_impl.dart';
 import 'package:app/core/service/notification/notification_service.dart';
-import 'package:app/injection/register_module.dart';
 import 'package:app/theme/color.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +8,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 @RoutePage()
 class NotificationPage extends StatelessWidget {
-  late final NotificationService notificationService = NotificationService(getIt<NotificationRepository>());
+  late final NotificationService notificationService = NotificationService(NotificationRepositoryImpl());
 
   Widget _notificationsListingBlocProvider(Widget child) {
     return BlocProvider<NotificationsListingBloc>(
@@ -17,7 +16,6 @@ class NotificationPage extends StatelessWidget {
       child: child,
     );
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -35,13 +33,22 @@ class _NotificationsListingView extends StatefulWidget {
 }
 
 class _NotificationsListingViewState extends State<_NotificationsListingView> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: LemonColor.black,
-      body: const Center(
-        child: Text('Notification'),
+      body: BlocBuilder<NotificationsListingBloc, NotificationsListingState>(
+        builder: (context, state) {
+          return Center(
+            child: Text(
+              state.when(
+                loading: () => "Im Loading",
+                fetched: (notifications) => "Fetched",
+                failure: () => "Failure",
+              ),
+            ),
+          );
+        },
       ),
     );
   }
