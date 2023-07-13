0b111cfc56a10c46c85996177c2b1d3e6746980f
Thinh CP <thinh@coderpush.com>
2023-07-13 16:04:14 +0700

    feat(wallet): define wallet specific dto, domain, bloc

--- lib/injection/register_module.config.dart
+++ lib/injection/register_module.config.dart
@@ -9,19 +9,23 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:app/core/application/auth/auth_bloc.dart' as _i14;
+import 'package:app/core/application/auth/auth_bloc.dart' as _i17;
 import 'package:app/core/data/event/repository/event_repository_impl.dart'
     as _i6;
 import 'package:app/core/data/post/post_repository_impl.dart' as _i8;
 import 'package:app/core/data/token/token_repository_impl.dart' as _i10;
 import 'package:app/core/data/user/user_repository_impl.dart' as _i12;
+import 'package:app/core/data/wallet/wallet_repository_impl.dart' as _i16;
 import 'package:app/core/domain/event/event_repository.dart' as _i5;
 import 'package:app/core/domain/post/post_repository.dart' as _i7;
 import 'package:app/core/domain/token/token_repository.dart' as _i9;
 import 'package:app/core/domain/user/user_repository.dart' as _i11;
+import 'package:app/core/domain/wallet/wallet_repository.dart' as _i15;
 import 'package:app/core/gql.dart' as _i3;
 import 'package:app/core/oauth.dart' as _i4;
 import 'package:app/core/service/user/user_service.dart' as _i13;
+import 'package:app/core/service/wallet_connect/wallet_connect_service.dart'
+    as _i14;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -44,8 +48,11 @@ extension GetItInjectableX on _i1.GetIt {
     gh.lazySingleton<_i9.TokenRepository>(() => _i10.TokenRepositoryImpl());
     gh.lazySingleton<_i11.UserRepository>(() => _i12.UserRepositoryImpl());
     gh.lazySingleton<_i13.UserService>(() => _i13.UserService());
-    gh.lazySingleton<_i14.AuthBloc>(
-        () => _i14.AuthBloc(userService: gh<_i13.UserService>()));
+    gh.lazySingleton<_i14.WalletConnectService>(
+        () => _i14.WalletConnectService());
+    gh.lazySingleton<_i15.WalletRepository>(() => _i16.WalletRepositoryImpl());
+    gh.lazySingleton<_i17.AuthBloc>(
+        () => _i17.AuthBloc(userService: gh<_i13.UserService>()));
     return this;
   }
 }
