f93e56d818f8f4ce1a7abd181a030c5f425141c2
Sieu Thai <>
2023-07-17 22:48:35 +0700

    feat(notification): add notification dto and entities

--- lib/core/data/notification/dtos/notification_dtos.dart
+++ lib/core/data/notification/dtos/notification_dtos.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/data/user/dtos/user_dtos.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'notification_dtos.freezed.dart';
@@ -5,13 +6,19 @@ part 'notification_dtos.g.dart';
 
 @freezed
 class NotificationDto with _$NotificationDto {
-  const factory NotificationDto({
-    required String id,
-    required String message,
-    required String type,
-    // Add other properties as needed
+  @JsonSerializable(explicitToJson: true)
+  factory NotificationDto({
+    @JsonKey(name: '_id') String? id,
+    @JsonKey(name: 'from_expanded') UserDto? fromExpanded,
+    
+    String? message,
+    String? type,
+    DateTime? stamp,
+    String? from,
+    bool? seen,
+    String? object_id,
+    String? object_type,
   }) = _NotificationDto;
 
-  factory NotificationDto.fromJson(Map<String, dynamic> json) =>
-      _$NotificationDtoFromJson(json);
+  factory NotificationDto.fromJson(Map<String, dynamic> json) => _$NotificationDtoFromJson(json);
 }
