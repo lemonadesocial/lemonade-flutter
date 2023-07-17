9088dd6c829d6bcfab56189591a5f99c11814bb3
Sieu Thai <>
2023-07-17 22:50:11 +0700

    feat(notification): add logic handle notification

---
+++ lib/core/utils/dialog_utils.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class DialogUtils {
+  static void showAlert({
+    required BuildContext context,
+    String? title,
+    String? message,
+    String? confirmText = 'OK',
+    Function()? onConfirm,
+  }) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: title != null ? Text(title) : null,
+          content: message != null ? Text(message) : null,
+          actions: [
+            TextButton(
+              child: Text(confirmText ?? "Ok"),
+              onPressed: () {
+                Navigator.of(context).pop();
+                if (onConfirm != null) {
+                  onConfirm();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
