ec0508f041137f4101e0c486523b4e0e4486e134
sieu-fk <sieu@firekamp.com>
2023-07-27 10:17:55 +0700

    refactor: resolve comment

--- lib/core/presentation/pages/home/home_page.dart
+++ lib/core/presentation/pages/home/home_page.dart
@@ -1,8 +1,5 @@
-import 'package:app/core/service/firebase/firebase_service.dart';
-import 'package:app/core/utils/navigation_utils.dart';
 import 'package:app/theme/color.dart';
 import 'package:auto_route/auto_route.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -15,21 +12,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageViewState extends State<HomePage> {
   
-  Future<void> setupInteractedMessage() async {
-    RemoteMessage? initialMessage =
-        await FirebaseMessaging.instance.getInitialMessage();
-    if (initialMessage != null) {
-      try {
-          String type = initialMessage.data['type']; 
-          String objectId = initialMessage.data['object_id']; 
-          String objectType = initialMessage.data['object_type'];
-          NavigationUtils.handleNotificationNavigate(context, type, objectType, objectId);
-        } catch (e) {
-          print("Error parsing JSON: $e");
-        }
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,14 +20,5 @@ class _HomePageViewState extends State<HomePage> {
         child: Text('Home'),
       ),
     );
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) async {
-      FirebaseService.setContext(context);
-    });
-    setupInteractedMessage();
   }
 }
--- lib/core/utils/navigation_utils.dart
+++ lib/core/utils/navigation_utils.dart
@@ -1,23 +1,25 @@
 import 'package:app/core/data/notification/notification_constants.dart';
-import 'package:app/core/domain/notification/entities/notification.dart' as NotificationEntities;
+import 'package:app/core/data/notification/notification_enums.dart';
 import 'package:app/core/utils/dialog_utils.dart';
 import 'package:app/router/app_router.gr.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
+
 class NavigationUtils {
 
   static handleNotificationNavigate(BuildContext context, String? type, String? objectType, String? objectId) {
-    switch(objectType) {
-      case "StoreOrder": 
+    NotificationObjectType? objectTypeEnum = objectTypeMap[objectType ?? ""];
+    switch(objectTypeEnum) {
+      case NotificationObjectType.StoreOrder: 
         DialogUtils.showAlert(context: context, title: "Redirect to store order");
         break;
-      case "Event":
+      case NotificationObjectType.Event:
         AutoRouter.of(context).navigate(
           EventDetailRoute(eventId: objectId ?? '', eventName: ''),
         );
         break;
-      case "User":
+      case NotificationObjectType.User:
         AutoRouter.of(context).navigate(
           ProfileRoute(userId: objectId ?? '')
         );
