b32d742fe1f7bcd4dc4fc22eb433b2781a448293
sieuhuflit <sieuhuflit@gmail.com>
2023-07-29 16:35:01 +0700

    feat(matrix): configure matrix for android platform

--- android/app/src/main/kotlin/com/lemonadesocial/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/lemonadesocial/app/MainActivity.kt
@@ -1,6 +1,32 @@
 package com.lemonadesocial.app
 
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterActivity() {
+import android.content.Context
+import androidx.multidex.MultiDex
+
+class MainActivity : FlutterActivity() {
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun provideFlutterEngine(context: Context): FlutterEngine? {
+        return provideEngine(this)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        // do nothing, because the engine was been configured in provideEngine
+    }
+
+    companion object {
+        var engine: FlutterEngine? = null
+        fun provideEngine(context: Context): FlutterEngine {
+            val eng = engine ?: FlutterEngine(context, emptyArray(), true, false)
+            engine = eng
+            return eng
+        }
+    }
 }
