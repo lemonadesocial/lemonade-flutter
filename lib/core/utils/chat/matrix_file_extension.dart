43f8da698b17ddca7f2ad467f0824877dfc8a04c
Thinh CP <thinh@coderpush.com>
2023-07-31 16:00:59 +0700

    feat(chat): remove redundant code

--- lib/core/utils/chat/matrix_file_extension.dart
+++ lib/core/utils/chat/matrix_file_extension.dart
@@ -1,20 +1,6 @@
-import 'dart:io';
 import 'package:matrix/matrix.dart';
-import 'package:path_provider/path_provider.dart';
 
 extension MatrixFileExtension on MatrixFile {
-  Future<String> getDownloadPathAndroid() async {
-    final directory = await getDownloadDirectoryAndroid();
-    return '${directory.path}/$name';
-  }
-
-  Future<Directory> getDownloadDirectoryAndroid() async {
-    final defaultDownloadDirectory = Directory('/storage/emulated/0/Download');
-    if (await defaultDownloadDirectory.exists()) {
-      return defaultDownloadDirectory;
-    }
-    return await getApplicationDocumentsDirectory();
-  }
 
   MatrixFile get detectFileType {
     if (msgType == MessageTypes.Image) {
