e395aa3496adb7cc66e57aa6a949a3579a0d4ee1
Sieu Thai <sieuthai@Sieus-MacBook-Pro.local>
2023-07-31 08:46:19 +0700

    refactor(chat): clean up

--- lib/core/utils/date_format_utils.dart
+++ lib/core/utils/date_format_utils.dart
@@ -23,9 +23,4 @@ class DateFormatUtils {
     if(date == null) return '';
     return DateFormat(monthYearOnlyFormat).format(date.toLocal());
   }
-
-  static String timeOnly(DateTime? date) {
-    if(date == null) return '';
-    return DateFormat(timeOnlyFormat).format(date.toLocal());
-  }
 }
