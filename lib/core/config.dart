a8b44c3360b4f08ee068e9a977c2087a194ba88b
sieuhuflit <sieuhuflit@gmail.com>
2023-08-16 22:18:41 +0700

    chore: update endpoint

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -30,6 +30,6 @@ class AppConfig {
 
   static const String pushNotificationsAppId = 'social.lemonade.app.staging';
   static const String pushNotificationsGatewayUrl =
-      'https://b369-115-76-94-123.ngrok.io/_matrix/push/v1/notify';
+      'https://c2cf-116-110-40-0.ngrok.io/_matrix/push/v1/notify';
   static const String pushNotificationsPusherFormat = 'event_id_only';
 }
