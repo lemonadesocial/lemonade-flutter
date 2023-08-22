430e40bdff8f72bcf8c72b69de84b413ac0e5a25
sieuhuflit <sieuhuflit@gmail.com>
2023-08-23 03:28:32 +0700

    docs: update readme

--- scripts/install_dev_env.sh
+++ scripts/install_dev_env.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 
 printMessage() {
-    printf "${YELLOW}Lemonade : $1${ENDCOLOR}\n"
+    printf "${YELLOW}🍋 Lemonade : $1${ENDCOLOR}\n"
+}
+
+printSuccess() {
+    printf "${GREEN}🍋 Lemonade : $1${ENDCOLOR}\n"
 }
 
 REPO_DIR=$(git rev-parse --show-toplevel)
@@ -14,8 +19,11 @@ git config core.hooksPath $REPO_DIR/.githooks
 
 # Install go-gitlint
 printMessage "Installing go-gitlint."
+
 GOLINT_FILENAME="go-gitlint_1.1.0_osx_x86_64.tar.gz"
 curl -L https://github.com/llorllale/go-gitlint/releases/download/1.1.0/${GOLINT_FILENAME} --output ${GOLINT_FILENAME}
 tar -zxv --directory $REPO_DIR/.githooks/. -f ${GOLINT_FILENAME} gitlint
 rm ${GOLINT_FILENAME}
+
+printSuccess "Install dev env successfully"
 
