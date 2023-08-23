fc0e47818a0cc33a6cc70a5c1642cbb2beaae7a5
sieuhuflit <sieuhuflit@gmail.com>
2023-08-24 03:31:51 +0700

    feat(bottom-bar): add animation for bottom bar

--- lib/core/presentation/widgets/bottom_bar/bottom_bar_widget.dart
+++ lib/core/presentation/widgets/bottom_bar/bottom_bar_widget.dart
@@ -8,14 +8,40 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hive/hive.dart';
 
 class BottomBar extends StatefulWidget {
   @override
   _BottomBarState createState() => _BottomBarState();
 }
 
-class _BottomBarState extends State<BottomBar> {
+class _BottomBarState extends State<BottomBar>
+    with SingleTickerProviderStateMixin {
   AppTab _selectedTab = AppTab.home;
+  late AnimationController _animationController;
+  late Animation<double> _animation;
+  bool _isTabChanged = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
+    _animation = Tween<double>(begin: 0, end: 1).animate(
+      CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.easeInOut,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,11 +75,34 @@ class _BottomBarState extends State<BottomBar> {
   }
 
   Widget _buildItem(BuildContext context, TabData tabData, bool isSelected) {
-    final colorScheme = Theme.of(context).colorScheme;
-    Widget icon;
+    final icon = _buildIcon(context, tabData);
+    return Expanded(
+      child: GestureDetector(
+        behavior: HitTestBehavior.opaque,
+        onTap: () {
+          _handleTabTap(context, tabData);
+        },
+        child: Container(
+          color: Colors.transparent,
+          padding: EdgeInsets.symmetric(
+            horizontal: Spacing.xSmall,
+            vertical: Spacing.xSmall,
+          ),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              _buildAnimatedContainer(isSelected),
+              icon, // The icon is outside the animated container
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
+  Widget _buildIcon(BuildContext context, TabData tabData) {
     if (tabData.tab == AppTab.profile) {
-      icon = BlocBuilder<AuthBloc, AuthState>(
+      return BlocBuilder<AuthBloc, AuthState>(
         builder: (context, authState) {
           if (authState is AuthStateAuthenticated) {
             return Center(
@@ -65,67 +114,61 @@ class _BottomBarState extends State<BottomBar> {
           } else if (authState is AuthStateProcessing) {
             return Loading.defaultLoading(context);
           } else {
+            final colorScheme = Theme.of(context).colorScheme;
             return Icon(Icons.person, color: colorScheme.onPrimary, size: 24);
           }
         },
       );
     } else {
-      icon = tabData.icon;
+      return tabData.icon;
     }
-    return Expanded(
-      child: GestureDetector(
-        behavior: HitTestBehavior.opaque,
-        onTap: () {
-          setState(() {
-            _selectedTab = tabData.tab;
-          });
-          AutoRouter.of(context)
-              .navigateNamed(tabData.route, includePrefixMatches: true);
-        },
-        child: Container(
-          color: Colors.transparent,
-          padding: EdgeInsets.symmetric(
-            horizontal: Spacing.xSmall,
-            vertical: Spacing.xSmall,
-          ),
-          child: Container(
-            width: 54.6,
-            height: 36,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(18),
-              color: isSelected ? LemonColor.white09 : Colors.transparent,
-            ),
-            padding: EdgeInsets.symmetric(vertical: Spacing.superExtraSmall),
-            child: icon,
-          ),
-        ),
-      ),
-    );
   }
-}
 
-class _ProfileAuthGuardItem extends StatelessWidget {
-  const _ProfileAuthGuardItem({
-    required this.authenticatedChild,
-    required this.unauthenticatedChild,
-    required this.processingChild,
-  });
+  void _handleTabTap(BuildContext context, TabData tabData) {
+    setState(() {
+      _isTabChanged = true;
+      _selectedTab = tabData.tab;
+    });
+    _animationController.reset();
+    _animationController.forward();
+    if (tabData.tab == AppTab.profile) {
+      final authState = BlocProvider.of<AuthBloc>(context).state;
+      if (authState is AuthStateAuthenticated) {
+        AutoRouter.of(context)
+            .navigateNamed(tabData.route, includePrefixMatches: true);
+      } else {
+        AutoRouter.of(context).navigateNamed('/login');
+      }
+    } else {
+      AutoRouter.of(context)
+          .navigateNamed(tabData.route, includePrefixMatches: true);
+    }
+  }
 
-  final Widget authenticatedChild;
-  final Widget unauthenticatedChild;
-  final Widget processingChild;
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocBuilder<AuthBloc, AuthState>(
-      builder: (context, authState) {
-        return authState.when(
-          authenticated: (_) {
-            return authenticatedChild;
-          },
-          unauthenticated: (_) => unauthenticatedChild,
-          unknown: () => unauthenticatedChild,
-          processing: () => processingChild,
+  Widget _buildAnimatedContainer(bool isSelected) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        if (_isTabChanged) {
+          return Transform.scale(
+            scale: _animation.value,
+            child: Container(
+              width: 54.6,
+              height: 36,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: isSelected ? LemonColor.white09 : Colors.transparent,
+              ),
+            ),
+          );
+        }
+        return Container(
+          width: 54.6,
+          height: 36,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(18),
+            color: isSelected ? LemonColor.white09 : Colors.transparent,
+          ),
         );
       },
     );
