5796fb0299ea35324ab5de2d316ef30111f0ae0f
Sieu Thai <sieuhuflit@gmail.com>
2023-08-30 15:52:59 +0700

    style: redesign linear gradient button (#97)
    ## Overview
    
    - Redesign linear gradient button
    
    ## Demo
    
    <img width="371" alt="Screenshot 2023-08-30 at 11 45 36"
    src="https://github.com/lemonadesocial/lemonade-flutter/assets/7247063/e62bd989-1608-45b4-b996-4da90d8a33eb">

--- lib/core/presentation/widgets/common/button/linear_gradient_button_widget.dart
+++ lib/core/presentation/widgets/common/button/linear_gradient_button_widget.dart
@@ -19,7 +19,7 @@ enum GradientButtonMode {
     }
   }
 
-  Color? get disableColor{
+  Color? get disableColor {
     switch (this) {
       case lavenderMode:
         return LemonColor.disableButtonLavender;
@@ -53,36 +53,39 @@ class LinearGradientButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 1.sw,
-      decoration: BoxDecoration(
-        borderRadius: radius ?? BorderRadius.circular(LemonRadius.xSmall),
-        gradient: LinearGradient(
-          begin: Alignment.topCenter,
-          end: Alignment.bottomCenter,
-          colors: mode.gradients,
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: LemonColor.shadow,
-            blurRadius: 4,
-            offset: const Offset(0, 2),
-          )
-        ],
-      ),
-      child: ElevatedButton(
-        onPressed: onTap,
-        style: ElevatedButton.styleFrom(
-          shape: RoundedRectangleBorder(
-            borderRadius: radius ?? BorderRadius.circular(LemonRadius.xSmall),
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        width: 1.sw,
+        padding: EdgeInsets.symmetric(vertical: Spacing.xSmall),
+        decoration: BoxDecoration(
+          borderRadius: radius ?? BorderRadius.circular(LemonRadius.xSmall),
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: mode.gradients,
           ),
-          padding: padding ?? EdgeInsets.symmetric(vertical: Spacing.xSmall),
-          backgroundColor: Colors.transparent,
-          disabledBackgroundColor: mode.disableColor,
         ),
-        child: Text(
-          label,
-          style: textStyle ?? Typo.medium.copyWith(fontWeight: FontWeight.w600),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: radius ?? BorderRadius.circular(LemonRadius.xSmall),
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 48.r,
+              ),
+              BoxShadow(
+                color: LemonColor.black.withOpacity(0.36),
+                offset: const Offset(0, 2),
+                blurRadius: 15.r,
+              ),
+            ],
+          ),
+          child: Text(
+            label,
+            textAlign: TextAlign.center,
+            style:
+                textStyle ?? Typo.medium.copyWith(fontWeight: FontWeight.w600),
+          ),
         ),
       ),
     );
