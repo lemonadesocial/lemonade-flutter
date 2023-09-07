82cb6793501ecd0cf17365a0019a77c0d7c15fbd
sieuhuflit <sieuhuflit@gmail.com>
2023-09-07 14:50:57 +0700

    chore: fix code format

--- lib/core/presentation/pages/home/views/list/home_newsfeed_list.dart
+++ lib/core/presentation/pages/home/views/list/home_newsfeed_list.dart
@@ -7,8 +7,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pull_to_refresh_flutter3/pull_to_refresh_flutter3.dart';
 
-import '../../../../widgets/bottom_bar/bottom_bar_widget.dart';
-
 class HomeNewsfeedListView extends StatelessWidget {
   const HomeNewsfeedListView({
     super.key,
