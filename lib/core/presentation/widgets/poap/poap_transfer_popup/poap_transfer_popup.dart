05502761a9849908aa8caf1faa5543fe36c18296
Thinh CP <thinh@coderpush.com>
2023-09-13 23:42:11 +0700

    feat(poap): implement transfer poap popup, transfer popap success popup, handle logic when transfer modification coming

---
+++ lib/core/presentation/widgets/poap/poap_transfer_popup/poap_transfer_popup.dart
@@ -0,0 +1,253 @@
+import 'package:app/core/application/poap/transfer_poap_bloc/transfer_poap_bloc.dart';
+import 'package:app/core/domain/poap/input/poap_input.dart';
+import 'package:app/core/domain/token/entities/token_entities.dart';
+import 'package:app/core/domain/poap/entities/poap_entities.dart';
+import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
+import 'package:app/core/presentation/widgets/image_placeholder_widget.dart';
+import 'package:app/core/presentation/widgets/lemon_text_field.dart';
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/core/utils/media_utils.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/gen/fonts.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:web3dart/web3dart.dart';
+
+class PoapTransferPopup extends StatelessWidget {
+  const PoapTransferPopup({
+    super.key,
+    this.token,
+    this.onClose,
+  });
+
+  final TokenDetail? token;
+  final Function()? onClose;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => TransferPoapBloc(),
+      child: _PoapTransferPopupView(
+        token: token,
+        onClose: onClose,
+      ),
+    );
+  }
+}
+
+class _PoapTransferPopupView extends StatefulWidget {
+  const _PoapTransferPopupView({
+    this.token,
+    this.onClose,
+  });
+
+  final TokenDetail? token;
+  final Function()? onClose;
+
+  @override
+  State<_PoapTransferPopupView> createState() => _PoapTransferPopupState();
+}
+
+class _PoapTransferPopupState extends State<_PoapTransferPopupView> {
+  String walletAddress = '';
+  bool isAddressValid = false;
+
+  bool validateAddress(String address) {
+    if (address.isEmpty) return false;
+
+    try {
+      EthereumAddress.fromHex(address);
+      return true;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  void onChange(String value) {
+    setState(() {
+      walletAddress = value;
+      isAddressValid = validateAddress(value);
+    });
+  }
+
+  void transfer() {
+    context.read<TransferPoapBloc>().add(
+          TransferPoapEvent.transfer(
+            input: TransferInput(
+              input: TransferArgsInput(
+                to: walletAddress,
+                tokenId: widget.token?.tokenId,
+              ),
+              network: widget.token?.network ?? '',
+              address: widget.token?.contract ?? '',
+            ),
+          ),
+        );
+  }
+
+  void onTransferSuccess(Transfer transfer) {
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final t = Translations.of(context);
+    final verticalOffset = MediaQuery.of(context).viewInsets.bottom * -0.5;
+    return BlocListener<TransferPoapBloc, TransferPoapState>(
+      listener: (context, state) {
+        state.maybeWhen(
+          orElse: () => null,
+          success: onTransferSuccess,
+        );
+      },
+      child: SingleChildScrollView(
+        child: AnimatedContainer(
+          padding: EdgeInsets.only(top: 78.w),
+          transform: Matrix4.translationValues(
+            0.0,
+            verticalOffset,
+            0.0,
+          ),
+          duration: const Duration(milliseconds: 100),
+          child: Center(
+            child: Dialog(
+              backgroundColor: colorScheme.primary,
+              insetPadding: EdgeInsets.only(
+                left: Spacing.smMedium,
+                right: Spacing.smMedium,
+              ),
+              child: SizedBox(
+                height: 0.68.sh,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                    LemonRadius.small,
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 339.w,
+                        child: Stack(
+                          children: [
+                            Positioned.fill(
+                              child: FutureBuilder(
+                                future: MediaUtils.getNftMedia(
+                                  widget.token?.metadata?.image,
+                                  widget.token?.metadata?.animation_url,
+                                ),
+                                builder: (context, snapshot) =>
+                                    CachedNetworkImage(
+                                  fit: BoxFit.cover,
+                                  imageUrl: snapshot.data?.url ?? '',
+                                  placeholder: (_, __) =>
+                                      ImagePlaceholder.defaultPlaceholder(
+                                    radius: BorderRadius.only(
+                                      topLeft:
+                                          Radius.circular(LemonRadius.small),
+                                      topRight:
+                                          Radius.circular(LemonRadius.small),
+                                    ),
+                                  ),
+                                  errorWidget: (_, __, ___) =>
+                                      ImagePlaceholder.defaultPlaceholder(
+                                    radius: BorderRadius.only(
+                                      topLeft:
+                                          Radius.circular(LemonRadius.small),
+                                      topRight:
+                                          Radius.circular(LemonRadius.small),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: InkWell(
+                                onTap: () => widget.onClose?.call(),
+                                child: Container(
+                                  width: 42.w,
+                                  height: 42.w,
+                                  margin: EdgeInsets.all(Spacing.xSmall),
+                                  decoration: BoxDecoration(
+                                    color:
+                                        colorScheme.primary.withOpacity(0.36),
+                                    borderRadius: BorderRadius.circular(42.w),
+                                  ),
+                                  child: Center(
+                                    child: ThemeSvgIcon(
+                                      color: colorScheme.onSecondary,
+                                      builder: (filter) => Assets.icons.icBack
+                                          .svg(colorFilter: filter),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          color: LemonColor.dialogBackground,
+                          padding: EdgeInsets.all(Spacing.medium),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                t.nft.transferCollectibleTo,
+                                style: Typo.extraMedium.copyWith(
+                                  fontWeight: FontWeight.w600,
+                                  fontFamily: FontFamily.switzerVariable,
+                                ),
+                              ),
+                              SizedBox(height: Spacing.xSmall),
+                              LemonTextField(
+                                hintText: t.nft.enterWalletId,
+                                onChange: onChange,
+                              ),
+                              SizedBox(height: Spacing.medium),
+                              BlocBuilder<TransferPoapBloc, TransferPoapState>(
+                                builder: (context, state) {
+                                  final isProcessing = state.maybeWhen(
+                                      loading: () => true, orElse: () => false);
+                                  return SizedBox(
+                                    height: 42.w,
+                                    child: Opacity(
+                                      opacity: isAddressValid && !isProcessing
+                                          ? 1
+                                          : 0.5,
+                                      child: LinearGradientButton(
+                                        label: isProcessing
+                                            ? '${t.common.processing}...'
+                                            : t.nft.transferCollectible,
+                                        mode: GradientButtonMode.lavenderMode,
+                                        onTap: isAddressValid || isProcessing
+                                            ? transfer
+                                            : null,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/core/presentation/widgets/poap/poap_transfer_success_poup/poap_transfer_success_popup.dart
@@ -0,0 +1,113 @@
+import 'package:app/core/domain/token/entities/token_entities.dart';
+import 'package:app/core/presentation/widgets/animation/ripple_animation.dart';
+import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/gen/fonts.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class PoapTransferSuccessPopup extends StatelessWidget {
+  const PoapTransferSuccessPopup({
+    super.key,
+    this.token,
+  });
+
+  final TokenDetail? token;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final t = Translations.of(context);
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(LemonRadius.small),
+      ),
+      backgroundColor: LemonColor.dialogBackground,
+      insetPadding: EdgeInsets.only(
+        left: Spacing.smMedium,
+        right: Spacing.smMedium,
+      ),
+      child: SizedBox(
+        height: 0.6465.sh,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(
+            LemonRadius.small,
+          ),
+          child: Stack(
+            children: [
+              Transform.translate(
+                offset: const Offset(0, -100),
+                child: Stack(
+                  children: [
+                    Align(
+                      alignment: Alignment.center,
+                      child: LayoutBuilder(
+                        builder: (context, constraints) => Transform.scale(
+                          scale: 1.8,
+                          child: RippleAnimation(
+                            size: constraints.maxWidth,
+                            color: const Color.fromRGBO(32, 41, 38, 1),
+                            scaleTween: Tween<double>(begin: 0.3, end: 1),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Assets.icons.icSuccess.svg(),
+                    ),
+                  ],
+                ),
+              ),
+              Transform.translate(
+                offset: Offset(0, -Spacing.medium),
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Flexible(
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(horizontal: Spacing.medium),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            t.nft.transferred,
+                            style: Typo.extraLarge.copyWith(
+                              fontWeight: FontWeight.w600,
+                              fontFamily: FontFamily.nohemiVariable,
+                            ),
+                          ),
+                          SizedBox(height: Spacing.extraSmall),
+                          Text(
+                            t.nft.transferSuccess(
+                                tokenName: token?.metadata?.name ?? ''),
+                            style: Typo.mediumPlus.copyWith(
+                              color: colorScheme.onSecondary,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          SizedBox(height: Spacing.medium),
+                          SizedBox(
+                            height: 42.w,
+                            child: LinearGradientButton(
+                              label: '${t.common.done}!',
+                              onTap: () => Navigator.of(context).pop(),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
