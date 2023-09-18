ae82a2e5d51fd29d9cf2a94e1ff2e105e7eac1b9
sieuhuflit <sieuhuflit@gmail.com>
2023-09-18 10:50:43 +0700

    feat(chat): implement direct chat flow

--- lib/core/application/chat/new_chat_bloc/new_chat_bloc.dart
+++ lib/core/application/chat/new_chat_bloc/new_chat_bloc.dart
@@ -1,8 +1,12 @@
+import 'package:app/core/presentation/widgets/future_loading_dialog.dart';
 import 'package:app/core/service/matrix/matrix_service.dart';
 import 'package:app/injection/register_module.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:matrix/matrix.dart';
+import 'package:matrix/matrix.dart' as sdk;
 
 part 'new_chat_bloc.freezed.dart';
 
@@ -11,7 +15,8 @@ class NewChatBloc extends Bloc<NewChatEvent, NewChatState> {
 
   NewChatBloc()
       : super(
-          const NewChatState(userSearchResult: null, selectedUsers: []),
+          const NewChatState(
+              userSearchResult: null, selectedUsers: [], isSearching: false),
         ) {
     on<NewChatEventSearchUsers>(_onSearchUsers);
     on<NewChatEventSelectUser>(_onSelectUser);
@@ -19,24 +24,67 @@ class NewChatBloc extends Bloc<NewChatEvent, NewChatState> {
   }
 
   _onSearchUsers(NewChatEventSearchUsers event, Emitter emit) async {
+    emit(state.copyWith(isSearching: true));
     var text = event.text;
-    final result = await matrixService.client.searchUserDirectory(
-      text!,
-      limit: 20,
-    );
-    emit(state.copyWith(userSearchResult: result));
+    try {
+      final result = await matrixService.client.searchUserDirectory(
+        text!,
+        limit: 20,
+      );
+      emit(state.copyWith(isSearching: false));
+      emit(state.copyWith(userSearchResult: result));
+    } catch (e) {
+      emit(state.copyWith(isSearching: false));
+    }
   }
 
   _onSelectUser(NewChatEventSelectUser event, Emitter emit) {
-    final selectedUsers = List<String>.from(state.selectedUsers);
-    selectedUsers.add(event.userId);
+    final selectedUsers = List<Profile>.from(state.selectedUsers);
+    selectedUsers.add(event.user);
     emit(state.copyWith(selectedUsers: selectedUsers));
   }
 
   _onDeselectUser(NewChatEventDeselectUser event, Emitter emit) {
-    final selectedUsers = List<String>.from(state.selectedUsers);
-    selectedUsers.remove(event.userId);
+    final selectedUsers = List<Profile>.from(state.selectedUsers);
+    selectedUsers.remove(event.user);
     emit(state.copyWith(selectedUsers: selectedUsers));
+  }
+
+  startChat(BuildContext context) async {
+    final List<String> selectedUserIds =
+        state.selectedUsers.map((profile) => profile.userId).toList();
+    // Direct 1 on 1 chat
+    if (selectedUserIds.length == 1) {
+      final roomID = await showFutureLoadingDialog<String>(
+        context: context,
+        future: () => matrixService.client.startDirectChat(
+          context.read<NewChatBloc>().state.selectedUsers[0].userId,
+        ),
+      );
+      if (roomID.error == null) {
+        AutoRouter.of(context).navigateNamed('/chat/detail/${roomID.result}');
+        await Future.delayed(const Duration(milliseconds: 400));
+        Navigator.of(context).pop();
+        return;
+      }
+    }
+    // Group chat
+    final roomID = await showFutureLoadingDialog(
+      context: context,
+      future: () async {
+        final roomId = await matrixService.client.createGroupChat(
+            visibility: sdk.Visibility.private,
+            preset: sdk.CreateRoomPreset.privateChat,
+            invite: selectedUserIds);
+        await matrixService.client.joinRoom(roomId);
+        return roomId;
+      },
+    );
+    if (roomID.error == null) {
+      AutoRouter.of(context).navigateNamed('/chat/detail/${roomID.result}');
+      await Future.delayed(const Duration(milliseconds: 400));
+      Navigator.of(context).pop();
+    }
   }
 }
 
@@ -47,18 +95,21 @@ class NewChatEvent with _$NewChatEvent {
   }) = NewChatEventSearchUsers;
 
   const factory NewChatEvent.selectUser({
-    required String userId,
+    required Profile user,
   }) = NewChatEventSelectUser;
 
   const factory NewChatEvent.deselectUser({
-    required String userId,
+    required Profile user,
   }) = NewChatEventDeselectUser;
+
+  const factory NewChatEvent.startChat() = NewChatEventStartChat;
 }
 
 @freezed
 class NewChatState with _$NewChatState {
   const factory NewChatState({
     required SearchUserDirectoryResponse? userSearchResult,
-    required List<String> selectedUsers,
+    required List<Profile> selectedUsers,
+    required bool isSearching,
   }) = _NewChatState;
 }
--- lib/core/presentation/pages/chat/new_chat/views/new_chat_view.dart
+++ lib/core/presentation/pages/chat/new_chat/views/new_chat_view.dart
@@ -2,6 +2,7 @@ import 'package:app/core/application/chat/new_chat_bloc/new_chat_bloc.dart';
 import 'package:app/core/presentation/pages/chat/new_chat/widgets/search_user_item.dart';
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
 import 'package:app/core/presentation/widgets/future_loading_dialog.dart';
+import 'package:app/core/presentation/widgets/loading_widget.dart';
 import 'package:app/core/utils/debouncer.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:auto_route/auto_route.dart';
@@ -9,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:matrix/matrix.dart' as sdk;
+import 'package:matrix/matrix.dart';
 
 class NewChatView extends StatelessWidget {
   final debouncer = Debouncer(milliseconds: 300);
@@ -31,40 +33,10 @@ class NewChatView extends StatelessWidget {
         backgroundColor: colorScheme.onPrimaryContainer,
         title: t.chat.newMessage,
         actions: [
-          InkWell(
+          StartButton(
             onTap: () async {
-              final roomID = await showFutureLoadingDialog(
-                context: context,
-                future: () async {
-                  final client =
-                      context.read<NewChatBloc>().matrixService.client;
-                  final roomId = await client.createGroupChat(
-                    visibility: sdk.Visibility.private,
-                    preset: sdk.CreateRoomPreset.privateChat,
-                    invite: context.read<NewChatBloc>().state.selectedUsers,
-                  );
-                  await client.joinRoom(roomId);
-                  return roomId;
-                },
-              );
-              if (roomID.error == null) {
-                AutoRouter.of(context)
-                    .navigateNamed('/chat/detail/${roomID.result}');
-                await Future.delayed(const Duration(milliseconds: 400));
-                Navigator.of(context).pop();
-              }
+              context.read<NewChatBloc>().startChat(context);
             },
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 15.w),
-              alignment: Alignment.centerRight,
-              child: Text(
-                "Start",
-                style: TextStyle(
-                  color: colorScheme.onSurface,
-                  fontSize: 16.0, // Adjust the font size as needed
-                ),
-              ),
-            ),
           ),
         ],
       ),
@@ -74,14 +46,12 @@ class NewChatView extends StatelessWidget {
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              TextField(
+              SearchField(
                 onChanged: (value) => onSearchChanged(context, value),
-                decoration: const InputDecoration(
-                  hintText: 'Search users',
-                  prefixIcon: Icon(Icons.search),
-                ),
               ),
-              if (newChatState.userSearchResult != null) ...[
+              if (newChatState.isSearching) ...[
+                Loading.defaultLoading(context),
+              ] else if (newChatState.userSearchResult != null) ...[
                 Expanded(
                   child: _buildUserList(newChatState, context),
                 ),
@@ -102,7 +72,8 @@ class NewChatView extends StatelessWidget {
         final userResult = newChatState.userSearchResult?.results[i];
         final userId = userResult?.userId ?? '';
         final isSelected = newChatState.selectedUsers.isNotEmpty &&
-            newChatState.selectedUsers.contains(userId);
+            newChatState.selectedUsers
+                .any((profile) => profile.userId == userId);
 
         return SearchUserItem(
           isSelected: isSelected,
@@ -113,16 +84,59 @@ class NewChatView extends StatelessWidget {
           onTap: () {
             if (isSelected) {
               context.read<NewChatBloc>().add(
-                    NewChatEvent.deselectUser(userId: userId),
+                    NewChatEvent.deselectUser(user: userResult!),
                   );
             } else {
               context.read<NewChatBloc>().add(
-                    NewChatEvent.selectUser(userId: userId),
+                    NewChatEvent.selectUser(user: userResult!),
                   );
             }
           },
         );
       },
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  final ValueChanged<String> onChanged;
+
+  const SearchField({super.key, required this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: onChanged,
+      decoration: const InputDecoration(
+        hintText: 'Search users',
+        prefixIcon: Icon(Icons.search),
+      ),
+    );
+  }
+}
+
+class StartButton extends StatelessWidget {
+  final Function() onTap;
+
+  const StartButton({super.key, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15.w),
+        alignment: Alignment.centerRight,
+        child: Text(
+          "Start",
+          style: TextStyle(
+            color: colorScheme.onSurface,
+            fontSize: 16.0, // Adjust the font size as needed
+          ),
+        ),
+      ),
     );
   }
 }
