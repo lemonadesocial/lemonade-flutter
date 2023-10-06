3d4ebd401f7f7fe3d810c449efcf0d0b1e77e019
Rontran636 <minhtq95@gmail.com>
2023-10-06 10:46:31 +0700

    feat(community): add navigation to profile page

--- lib/core/presentation/pages/community/widgets/community_user_tile.dart
+++ lib/core/presentation/pages/community/widgets/community_user_tile.dart
@@ -1,8 +1,10 @@
 import 'package:app/core/domain/community/community_user/community_user.dart';
 import 'package:app/core/presentation/widgets/lemon_circle_avatar_widget.dart';
+import 'package:app/router/app_router.gr.dart';
 import 'package:app/theme/color.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class CommunityUserTile extends StatelessWidget {
@@ -16,38 +18,41 @@ class CommunityUserTile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      padding: EdgeInsets.all(Spacing.small),
-      decoration: BoxDecoration(
-        color: LemonColor.atomicBlack,
-        borderRadius: BorderRadius.circular(LemonRadius.small),
-      ),
-      child: Row(
-        children: [
-          LemonCircleAvatar(url: user.imageAvatar ?? ''),
-          SizedBox(width: Spacing.xSmall),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  user.displayName ?? user.userName ?? '',
-                  style: Typo.medium.copyWith(
-                    fontWeight: FontWeight.w600,
-                    color: colorScheme.onPrimary,
+    return InkWell(
+      onTap: () => context.router.push(ProfileRoute(userId: user.id)),
+      child: Container(
+        padding: EdgeInsets.all(Spacing.small),
+        decoration: BoxDecoration(
+          color: LemonColor.atomicBlack,
+          borderRadius: BorderRadius.circular(LemonRadius.small),
+        ),
+        child: Row(
+          children: [
+            LemonCircleAvatar(url: user.imageAvatar ?? ''),
+            SizedBox(width: Spacing.xSmall),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    user.displayName ?? user.userName ?? '',
+                    style: Typo.medium.copyWith(
+                      fontWeight: FontWeight.w600,
+                      color: colorScheme.onPrimary,
+                    ),
                   ),
-                ),
-                Text(
-                  user.userName ?? '',
-                  style: Typo.small.copyWith(
-                    fontWeight: FontWeight.w400,
-                    color: colorScheme.onPrimary,
+                  Text(
+                    user.userName ?? '',
+                    style: Typo.small.copyWith(
+                      fontWeight: FontWeight.w400,
+                      color: colorScheme.onPrimary,
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
