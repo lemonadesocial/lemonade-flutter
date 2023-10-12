2826cc6db9a3dfa19ad501c218568c148b247247
Minh Tran (Ron) <108661349+roncoderpush@users.noreply.github.com>
2023-10-12 14:14:08 +0700

    feat(edit profile): add coming dialog for social edit profile (#199)
    ## What:
    - Handle coming dialog for social edit profile. These raw string will be
    replaced when we apply the feature so there no need to include it in
    localization.
    
    ## Screenshot:
    ![Simulator Screenshot - Registered iPhone 14 Pro Max - 2023-10-12 at 13
    34
    47](https://github.com/lemonadesocial/lemonade-flutter/assets/108661349/cfa9170f-472f-49fe-9e59-d2d73f63a294)
    
    ---------
    
    Co-authored-by: Rontran636 <minhtq95@gmail.com>

--- lib/core/presentation/pages/edit_profile/widgets/edit_profile_social_tile.dart
+++ lib/core/presentation/pages/edit_profile/widgets/edit_profile_social_tile.dart
@@ -8,48 +8,54 @@ class EditProfileSocialTile extends StatelessWidget {
     Key? key,
     required this.label,
     required this.leadingIcon,
-    required this.onClick,
+    required this.onTap,
+    this.content,
   }) : super(key: key);
 
   final String label;
   final Widget leadingIcon;
-  final VoidCallback onClick;
+  final VoidCallback onTap;
+  final String? content;
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          label,
-          style: Typo.medium.copyWith(
-            fontWeight: FontWeight.normal,
-            color: colorScheme.onPrimary.withOpacity(0.36),
+    return InkWell(
+      onTap: onTap,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: Typo.medium.copyWith(
+              fontWeight: FontWeight.normal,
+              color: colorScheme.onPrimary.withOpacity(0.36),
+            ),
           ),
-        ),
-        SizedBox(height: Spacing.superExtraSmall),
-        Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12.r),
-            border: Border.all(color: colorScheme.outlineVariant),
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 18.w),
-          child: Row(
-            children: [
-              leadingIcon,
-              SizedBox(width: Spacing.xSmall),
-              Text(
-                'facebook.com',
-                style: Typo.mediumPlus.copyWith(
-                  fontWeight: FontWeight.normal,
+          SizedBox(height: Spacing.superExtraSmall),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12.r),
+              border: Border.all(color: colorScheme.outlineVariant),
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 15.w, vertical: 18.w),
+            child: Row(
+              children: [
+                leadingIcon,
+                SizedBox(width: Spacing.xSmall),
+                Text(
+                  content ?? '',
+                  style: Typo.mediumPlus.copyWith(
+                    fontWeight: FontWeight.normal,
+                    color: colorScheme.outlineVariant,
+                  ),
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        )
-      ],
+        ],
+      ),
     );
   }
 }
