e081c72d7a3358b315cba83f2b2663da2b96a4c7
Sieu Thai <sieuhuflit@gmail.com>
2023-10-24 10:26:28 +0700

    feat(shorebird): Setup shorebird codepush (#229)
    ## Overview
    
    - Install and setup Shorebird: https://shorebird.dev/
    - Enhance CI/CD for build release shorebird when triggering tag
    **(Example: v1.0.0-rc.1)**
    - Enhance CI/CD for patch bugs using shorebird when triggering tag
    **(Example: v1.0.0+141-patch-ios.1)**
    - Add check version shorebird when resume app
    
    ---------
    
    Co-authored-by: Sieu Thai <sieuthai@Sieus-MacBook-Pro.local>
    Co-authored-by: Sieu Thai <sieuthai@192.168.31.85>
    Co-authored-by: Sieu Thai <>

--- lib/injection/register_module.config.dart
+++ lib/injection/register_module.config.dart
@@ -9,7 +9,7 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:app/core/application/auth/auth_bloc.dart' as _i38;
+import 'package:app/core/application/auth/auth_bloc.dart' as _i39;
 import 'package:app/core/application/chat/new_chat_bloc/new_chat_bloc.dart'
     as _i20;
 import 'package:app/core/data/badge/repository/badge_respository_impl.dart'
@@ -26,9 +26,9 @@ import 'package:app/core/data/post/newsfeed_repository_impl.dart' as _i22;
 import 'package:app/core/data/post/post_repository_impl.dart' as _i26;
 import 'package:app/core/data/report/repository/report_repository_impl.dart'
     as _i28;
-import 'package:app/core/data/token/token_repository_impl.dart' as _i31;
-import 'package:app/core/data/user/user_repository_impl.dart' as _i33;
-import 'package:app/core/data/wallet/wallet_repository_impl.dart' as _i37;
+import 'package:app/core/data/token/token_repository_impl.dart' as _i32;
+import 'package:app/core/data/user/user_repository_impl.dart' as _i34;
+import 'package:app/core/data/wallet/wallet_repository_impl.dart' as _i38;
 import 'package:app/core/domain/badge/badge_repository.dart' as _i6;
 import 'package:app/core/domain/community/community_repository.dart' as _i9;
 import 'package:app/core/domain/event/event_repository.dart' as _i13;
@@ -40,18 +40,19 @@ import 'package:app/core/domain/newsfeed/newsfeed_repository.dart' as _i21;
 import 'package:app/core/domain/poap/poap_repository.dart' as _i23;
 import 'package:app/core/domain/post/post_repository.dart' as _i25;
 import 'package:app/core/domain/report/report_repository.dart' as _i27;
-import 'package:app/core/domain/token/token_repository.dart' as _i30;
-import 'package:app/core/domain/user/user_repository.dart' as _i32;
-import 'package:app/core/domain/wallet/wallet_repository.dart' as _i36;
+import 'package:app/core/domain/token/token_repository.dart' as _i31;
+import 'package:app/core/domain/user/user_repository.dart' as _i33;
+import 'package:app/core/domain/wallet/wallet_repository.dart' as _i37;
 import 'package:app/core/oauth/oauth.dart' as _i4;
 import 'package:app/core/service/auth/auth_service.dart' as _i5;
 import 'package:app/core/service/badge/badge_service.dart' as _i8;
 import 'package:app/core/service/firebase/firebase_service.dart' as _i17;
 import 'package:app/core/service/matrix/matrix_service.dart' as _i19;
 import 'package:app/core/service/shake/shake_service.dart' as _i29;
-import 'package:app/core/service/user/user_service.dart' as _i34;
+import 'package:app/core/service/shorebird_codepush_service.dart' as _i30;
+import 'package:app/core/service/user/user_service.dart' as _i35;
 import 'package:app/core/service/wallet_connect/wallet_connect_service.dart'
-    as _i35;
+    as _i36;
 import 'package:app/core/utils/gql/gql.dart' as _i3;
 import 'package:app/core/utils/location_utils.dart' as _i18;
 import 'package:get_it/get_it.dart' as _i1;
@@ -91,15 +92,17 @@ extension GetItInjectableX on _i1.GetIt {
     gh.lazySingleton<_i25.PostRepository>(() => _i26.PostRepositoryImpl());
     gh.lazySingleton<_i27.ReportRepository>(() => _i28.ReportRepositoryImpl());
     gh.lazySingleton<_i29.ShakeService>(() => _i29.ShakeService());
-    gh.lazySingleton<_i30.TokenRepository>(() => _i31.TokenRepositoryImpl());
-    gh.lazySingleton<_i32.UserRepository>(() => _i33.UserRepositoryImpl());
-    gh.lazySingleton<_i34.UserService>(() => _i34.UserService());
-    gh.lazySingleton<_i35.WalletConnectService>(
-        () => _i35.WalletConnectService());
+    gh.lazySingleton<_i30.ShorebirdCodePushService>(
+        () => _i30.ShorebirdCodePushService());
+    gh.lazySingleton<_i31.TokenRepository>(() => _i32.TokenRepositoryImpl());
+    gh.lazySingleton<_i33.UserRepository>(() => _i34.UserRepositoryImpl());
+    gh.lazySingleton<_i35.UserService>(() => _i35.UserService());
+    gh.lazySingleton<_i36.WalletConnectService>(
+        () => _i36.WalletConnectService());
     gh.lazySingleton<_i3.WalletGQL>(() => _i3.WalletGQL());
-    gh.lazySingleton<_i36.WalletRepository>(() => _i37.WalletRepositoryImpl());
-    gh.lazySingleton<_i38.AuthBloc>(() => _i38.AuthBloc(
-          userService: gh<_i34.UserService>(),
+    gh.lazySingleton<_i37.WalletRepository>(() => _i38.WalletRepositoryImpl());
+    gh.lazySingleton<_i39.AuthBloc>(() => _i39.AuthBloc(
+          userService: gh<_i35.UserService>(),
           authService: gh<_i5.AuthService>(),
         ));
     return this;
