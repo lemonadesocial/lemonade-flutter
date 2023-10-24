49a531d774dd26893d7235a3799294a1930fd704
sieuhuflit <sieuhuflit@gmail.com>
2023-10-25 02:41:41 +0700

    chore: comment code out

--- lib/core/presentation/widgets/common/drawer/lemon_drawer.dart
+++ lib/core/presentation/widgets/common/drawer/lemon_drawer.dart
@@ -45,6 +45,15 @@ class LemonDrawer extends StatelessWidget {
           children: [
             SizedBox(height: Spacing.superExtraSmall),
             ...[
+              // DrawerItem(
+              //   icon: Assets.icons.icBank,
+              //   label: t.common.vault,
+              //   featureAvailable: false,
+              //   onPressed: () {
+              //     Vibrate.feedback(FeedbackType.light);
+              //     showComingSoonDialog(context);
+              //   },
+              // ),
               DrawerItem(
                 icon: Assets.icons.icPeopleAlt,
                 label: t.common.community,
