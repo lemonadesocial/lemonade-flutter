aac56fda73a3b6eb7b3a2c75609ac8908a8fa8d6
Mai Thinh <thinh@coderpush.com>
2023-10-25 02:04:03 +0700

    feat(badge): add location request popup before request location permission (#230)
    # What ?
    - add location request popup before requesting location permission
    
    # Screenshot
    
    
    https://github.com/lemonadesocial/lemonade-flutter/assets/28992487/f946b85f-2267-41c7-9e77-4e3b4db1c6ae
    
    
    
    https://github.com/lemonadesocial/lemonade-flutter/assets/28992487/d9209731-87c0-46f4-a3b7-e5d69259879b

--- lib/core/presentation/widgets/common/dialog/lemon_alert_dialog.dart
+++ lib/core/presentation/widgets/common/dialog/lemon_alert_dialog.dart
@@ -1,6 +1,8 @@
 import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
+import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/theme/color.dart';
+import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +10,14 @@ class LemonAlertDialog extends StatelessWidget {
   final Widget child;
   final String? buttonLabel;
   final Function()? onClose;
+  final bool? closable;
 
   const LemonAlertDialog({
     super.key,
     required this.child,
     this.buttonLabel,
     this.onClose,
+    this.closable = false,
   });
 
   @override
@@ -30,6 +34,21 @@ class LemonAlertDialog extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            if (closable!)
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Assets.icons.icClose.svg(
+                      width: Sizing.small,
+                      height: Sizing.small,
+                    ),
+                  )
+                ],
+              ),
             child,
             SizedBox(height: Spacing.small),
             LinearGradientButton(
