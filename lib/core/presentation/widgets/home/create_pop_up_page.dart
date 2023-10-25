ba31aa995339b6611065e72d05344f5e84bd9767
Sieu Thai <sieuhuflit@gmail.com>
2023-10-25 14:15:26 +0700

    fix: adjust height factor of create popup (#235)
    ## Overview
    
    - Adjust height factor of create popup
    - Handle Ipad UI
    - Remove landscape mode of iOS
    
    ## Demo
    ![Screenshot 2023-10-25 at 14 08
    34](https://github.com/lemonadesocial/lemonade-flutter/assets/7247063/5fc1e23d-4517-4b39-9ebc-381b889ca4e7)
    
    On iPad device
    
    ![simulator_screenshot_6C61C123-CA85-4057-A0B5-DD51B82033F5](https://github.com/lemonadesocial/lemonade-flutter/assets/7247063/340b52d6-431b-4588-9efa-3d151a1894f7)

--- lib/core/presentation/widgets/home/create_pop_up_page.dart
+++ lib/core/presentation/widgets/home/create_pop_up_page.dart
@@ -77,14 +77,15 @@ class CreatePopUpPage extends StatelessWidget with LemonBottomSheet {
                 ),
               ),
             ),
-            SizedBox(height: Spacing.xSmall),
-            CreatePopUpTile(
-              colors: CreatePopupGradient.poap.colors,
-              label: t.home.poap,
-              content: t.home.poapDesc,
-              suffixIcon: Assets.icons.icCreatePoap.svg(),
-            ),
-            SizedBox(height: Spacing.xSmall),
+            // TODO: Temporary comment, maybe re-open in future
+            // SizedBox(height: Spacing.xSmall),
+            // CreatePopUpTile(
+            //   colors: CreatePopupGradient.poap.colors,
+            //   label: t.home.poap,
+            //   content: t.home.poapDesc,
+            //   suffixIcon: Assets.icons.icCreatePoap.svg(),
+            // ),
+            // SizedBox(height: Spacing.xSmall),
             // CreatePopUpTile(
             //   colors: CreatePopupGradient.collectible.colors,
             //   label: t.home.collectible,
