6a9ebe0093df818ae2e4dcdb06fa954f0f1b0d22
Mai Thinh <thinh@coderpush.com>
2023-10-30 09:42:24 +0700

    feat(notification): implement delete notification (#242)
    # What ?
    - Implement delete notification

--- lib/core/presentation/pages/notification/widgets/notification_card_widget.dart
+++ lib/core/presentation/pages/notification/widgets/notification_card_widget.dart
@@ -1,4 +1,7 @@
+import 'package:app/core/application/notification/delete_notifications_bloc/delete_notifications_bloc.dart';
 import 'package:app/core/data/notification/notification_constants.dart';
+import 'package:app/core/domain/notification/input/delete_notifications_input.dart';
+import 'package:app/core/presentation/pages/notification/widgets/notification_slidable_widget.dart';
 import 'package:app/core/utils/image_utils.dart';
 import 'package:flutter/material.dart';
 import 'package:app/core/presentation/widgets/lemon_circle_avatar_widget.dart';
@@ -6,35 +9,107 @@ import 'package:app/theme/typo.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/core/domain/notification/entities/notification.dart'
     as notification_entities;
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:timeago/timeago.dart' as timeago;
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 import 'package:app/gen/assets.gen.dart';
 
 class NotificationCard extends StatelessWidget {
   final notification_entities.Notification notification;
+  final int index;
   final Function()? onTap;
+  final Function(
+    int index,
+    notification_entities.Notification notification,
+    bool? isDismiss,
+  )? onRemove;
 
   const NotificationCard({
     super.key,
+    required this.index,
     required this.notification,
     this.onTap,
+    this.onRemove,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => DeleteNotificationsBloc(),
+      child: NotificationCardView(
+        index: index,
+        notification: notification,
+        onTap: onTap,
+        onRemove: onRemove,
+      ),
+    );
+  }
+}
+
+class NotificationCardView extends StatelessWidget {
+  final notification_entities.Notification notification;
+  final int index;
+  final Function()? onTap;
+  final Function(
+    int index,
+    notification_entities.Notification notification,
+    bool? isDismiss,
+  )? onRemove;
+
+  const NotificationCardView({
+    super.key,
+    required this.index,
+    required this.notification,
+    this.onTap,
+    this.onRemove,
   });
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: EdgeInsets.all(Spacing.small),
-        decoration: BoxDecoration(
-          border: Border(bottom: BorderSide(color: colorScheme.outline)),
-        ),
-        child: Row(
-          children: [
-            _buildAvatar(),
-            Expanded(child: _buildMessage(colorScheme)),
-          ],
+    return NotificationSlidable(
+      id: notification.id ?? '',
+      onRemove: () {
+        context.read<DeleteNotificationsBloc>().add(
+              DeleteNotificationsEvent.delete(
+                input: DeleteNotificationsInput(
+                  ids: [notification.id ?? ''],
+                ),
+              ),
+            );
+        onRemove?.call(
+          index,
+          notification,
+          false,
+        );
+      },
+      onDismissed: () {
+        context.read<DeleteNotificationsBloc>().add(
+              DeleteNotificationsEvent.delete(
+                input: DeleteNotificationsInput(
+                  ids: [notification.id ?? ''],
+                ),
+              ),
+            );
+        onRemove?.call(
+          index,
+          notification,
+          true,
+        );
+      },
+      child: GestureDetector(
+        onTap: onTap,
+        child: Container(
+          padding: EdgeInsets.all(Spacing.small),
+          decoration: BoxDecoration(
+            border: Border(bottom: BorderSide(color: colorScheme.outline)),
+          ),
+          child: Row(
+            children: [
+              _buildAvatar(),
+              Expanded(child: _buildMessage(colorScheme)),
+            ],
+          ),
         ),
       ),
     );
