904d4e4d6ecc5a45420ebfc958fcbd77356e67b9
Sieu Thai <sieuhuflit@gmail.com>
2023-10-30 11:49:37 +0700

    feat(discover): Add communities section in discover (#239)
    ## Overview
    
    - Add communities section in discover screen
    
    ## Demo
    
    <img width="400" alt="Screenshot 2023-10-26 at 09 23 17"
    src="https://github.com/lemonadesocial/lemonade-flutter/assets/7247063/158b413e-8a25-4f7c-be34-6fe0ba181a75">

--- lib/core/data/community/community_query.dart
+++ lib/core/data/community/community_query.dart
@@ -88,3 +88,14 @@ final getUserFollower = gql('''
   }
 }
 ''');
+
+final getUsersSpotlightQuery = gql('''
+  $baseUserFragment
+  
+  query () {
+    getUsersSpotlight() {
+      ...baseUserFragment
+      __typename
+    }
+  }
+''');
