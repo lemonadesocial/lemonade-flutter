e19c242d6b0a9c62469e91cdc1f1157092ddf9f8
sieuhuflit <sieuhuflit@gmail.com>
2023-11-10 13:05:41 +0700

    feat(chat-ai): add widgets for chat ai

---
+++ lib/core/presentation/pages/ai/ai_page.dart
@@ -0,0 +1,143 @@
+import 'package:app/client/ai_client.dart';
+import 'package:app/core/application/auth/auth_bloc.dart';
+import 'package:app/core/config.dart';
+import 'package:app/core/domain/user/entities/user.dart';
+import 'package:app/core/presentation/widgets/ai/ai_chat_card.dart';
+import 'package:app/core/presentation/widgets/ai/ai_chat_composer.dart';
+import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
+import 'package:app/core/presentation/widgets/lemon_circle_avatar_widget.dart';
+import 'package:app/core/presentation/widgets/loading_widget.dart';
+import 'package:app/core/utils/avatar_utils.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/graphql/ai/__generated__/run.req.gql.dart';
+import 'package:app/injection/register_module.dart';
+import 'package:app/schemas/ai/__generated__/schema.schema.gql.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vibrate/flutter_vibrate.dart';
+
+class AIChatMessage {
+  final String text;
+  final bool isUser;
+
+  AIChatMessage(this.text, this.isUser);
+}
+
+@RoutePage()
+class AIPage extends StatefulWidget {
+  const AIPage({Key? key}) : super(key: key);
+
+  @override
+  AIPageState createState() => AIPageState();
+}
+
+class AIPageState extends State<AIPage> {
+  final client = getIt<AIClient>().client;
+  final ScrollController _scrollController = ScrollController();
+  bool _loading = false;
+  List<AIChatMessage> messages = [
+    AIChatMessage(
+      "I’m Lulu, your creative and helpful collaborator. I have limitations and won’t always get it right, but your feedback will help me improve. What would you like to create today?",
+      false,
+    ),
+  ];
+
+  final TextEditingController _textController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void send(String text) {
+    try {
+      Vibrate.feedback(FeedbackType.light);
+      FocusScope.of(context).unfocus();
+      if (_textController.text.trim().isEmpty) return;
+      _textController.clear();
+      setState(() {
+        _loading = true;
+        messages.insert(
+          messages.length,
+          AIChatMessage(text, true),
+        );
+      });
+
+      final config = GObjectIdBuilder();
+      config.value = AppConfig.aiConfig;
+      final createPostReq = GRunReq(
+        (b) => b
+          ..vars.config = config
+          ..vars.message = text,
+      );
+      client.request(createPostReq).listen((event) {
+        setState(() {
+          _loading = false;
+          messages.insert(
+            messages.length,
+            AIChatMessage(event.data!.run.message, false),
+          );
+        });
+        _scrollToEnd();
+      });
+    } catch (e) {
+      if (kDebugMode) {
+        print("error : $e");
+      }
+    }
+  }
+
+  void _scrollToEnd() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: const LemonAppBar(),
+      backgroundColor: colorScheme.primary,
+      body: Container(
+        decoration: BoxDecoration(
+          color: LemonColor.atomicBlack,
+          borderRadius: BorderRadius.circular(20.r),
+        ),
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: ListView.builder(
+                controller: _scrollController,
+                padding: EdgeInsets.symmetric(
+                  vertical: 10.h,
+                  horizontal: 10.w,
+                ),
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return AIChatCard(message: messages[index]);
+                },
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: AIChatComposer(
+                textController: _textController,
+                onSend: send,
+                loading: _loading,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/core/presentation/widgets/ai/ai_chat_composer.dart
@@ -0,0 +1,114 @@
+import 'package:app/core/presentation/widgets/loading_widget.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AIChatComposer extends StatelessWidget {
+  const AIChatComposer({
+    super.key,
+    this.loading,
+    required this.onSend,
+    required this.textController,
+  });
+
+  final bool? loading;
+  final TextEditingController textController;
+  final Function(String text) onSend;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).cardColor),
+      child: Container(
+        decoration: BoxDecoration(
+          color: LemonColor.atomicBlack,
+          border: Border(
+            top: BorderSide(
+              width: 1.h,
+              color: colorScheme.onPrimary.withOpacity(0.06),
+            ),
+          ),
+        ),
+        padding:
+            EdgeInsets.only(left: 15.w, right: 15.w, top: 15.h, bottom: 12.h),
+        child: Row(
+          children: [
+            Container(
+              width: 42.w,
+              height: 42.h,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: Assets.images.icChatAiBot.provider(),
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 6.w,
+            ),
+            Expanded(
+              child: TextField(
+                controller: textController,
+                onSubmitted: onSend,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: Spacing.extraSmall,
+                    horizontal: Spacing.smMedium,
+                  ),
+                  hintText: 'Enter your prompt here',
+                  hintStyle: Typo.medium.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.18),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(24.r),
+                    borderSide: BorderSide(
+                      color: colorScheme.outline,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(21),
+                    borderSide: BorderSide(
+                      color: colorScheme.outline,
+                    ),
+                  ),
+                  suffixIcon: _buildSendbutton(context),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSendbutton(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 4.0),
+      child: loading == true
+          ? Transform.scale(
+              scale: 0.5,
+              child: const SizedBox(
+                height: 24,
+                width: 24,
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          : IconButton(
+              icon: Icon(
+                Icons.send_rounded,
+                color: LemonColor.babyPurple,
+                size: 24.w,
+              ),
+              onPressed: () => onSend(textController.text),
+            ),
+    );
+  }
+}
