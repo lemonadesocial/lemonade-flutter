63c00cf4da8dd945ff16424cf87bbbc886b0be03
Thinh CP <thinh@coderpush.com>
2023-11-28 16:59:57 +0700

    feat(vault): add vault bottom sheet ui

---
+++ lib/core/presentation/pages/vault/vaults_listing_page/views/add_vault_bottomsheet.dart
@@ -0,0 +1,204 @@
+import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
+import 'package:app/core/presentation/widgets/common/bottomsheet/lemon_snap_bottom_sheet_widget.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/gen/fonts.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/sizing.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
+import 'package:flutter/material.dart';
+
+class AddVaultBottomSheet extends StatelessWidget {
+  const AddVaultBottomSheet({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Translations.of(context);
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return LemonSnapBottomSheet(
+      defaultSnapSize: 0.71,
+      backgroundColor: LemonColor.atomicBlack,
+      builder: (scrollController) => SafeArea(
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(LemonRadius.large),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              LemonAppBar(
+                title: "",
+                backgroundColor: LemonColor.atomicBlack,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: Spacing.smMedium),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      t.vault.createVaultBottomSheet.title,
+                      style: Typo.extraLarge.copyWith(
+                        fontWeight: FontWeight.w600,
+                        fontFamily: FontFamily.nohemiVariable,
+                      ),
+                    ),
+                    SizedBox(height: Spacing.superExtraSmall),
+                    Text(
+                      t.vault.createVaultBottomSheet.subTitle,
+                      style: Typo.mediumPlus.copyWith(
+                        color: colorScheme.onSecondary,
+                      ),
+                    ),
+                    SizedBox(height: Spacing.medium),
+                    _CreateVaultOptionItem(
+                      title: t.vault.vaultType.individual,
+                      description:
+                          t.vault.createVaultBottomSheet.individualDescription,
+                      icon: Assets.icons.icProfile.svg(
+                        width: Sizing.small,
+                        height: Sizing.small,
+                      ),
+                      iconBackground: colorScheme.onPrimary.withOpacity(0.06),
+                      gradientColors: const [
+                        Color(0xff232323),
+                        Color(0xff252525),
+                        Color(0xff282828),
+                        Color(0xff343434),
+                      ],
+                    ),
+                    SizedBox(height: Spacing.xSmall),
+                    _CreateVaultOptionItem(
+                      title: t.vault.vaultType.community,
+                      description:
+                          t.vault.createVaultBottomSheet.communityDescription,
+                      icon: Assets.icons.icGuests.svg(
+                        width: Sizing.small,
+                        height: Sizing.small,
+                      ),
+                      iconBackground: LemonColor.paleViolet18,
+                      gradientColors: const [
+                        Color(0xff232323),
+                        Color(0xff262626),
+                        Color(0xff383442),
+                        Color(0xff484255),
+                      ],
+                    ),
+                    SizedBox(height: Spacing.medium),
+                    InkWell(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            t.vault.createVaultBottomSheet.alreadyHaveVault,
+                            style: Typo.medium.copyWith(
+                              color: colorScheme.onSecondary,
+                            ),
+                          ),
+                          SizedBox(width: Spacing.superExtraSmall),
+                          Text(
+                            t.vault.createVaultBottomSheet.restore,
+                            style: Typo.medium.copyWith(
+                              color: LemonColor.paleViolet,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CreateVaultOptionItem extends StatelessWidget {
+  final List<Color> gradientColors;
+  final Color iconBackground;
+  final Widget icon;
+  final String title;
+  final String description;
+
+  const _CreateVaultOptionItem({
+    required this.title,
+    required this.description,
+    required this.gradientColors,
+    required this.iconBackground,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Translations.of(context);
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+      padding: EdgeInsets.all(Spacing.medium),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(LemonRadius.normal),
+        gradient: LinearGradient(
+          stops: const [0.25, 0.5, 0.75, 1],
+          colors: gradientColors,
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: Sizing.large,
+            height: Sizing.large,
+            decoration: BoxDecoration(
+              color: iconBackground,
+              borderRadius: BorderRadius.circular(Sizing.large),
+            ),
+            child: Center(
+              child: icon,
+            ),
+          ),
+          SizedBox(width: Spacing.smMedium),
+          Flexible(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  t.vault.vaultType.individual,
+                  style: Typo.mediumPlus.copyWith(
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(height: Spacing.superExtraSmall),
+                Text(
+                  t.vault.createVaultBottomSheet.individualDescription,
+                  style: Typo.small.copyWith(
+                    color: colorScheme.onSecondary,
+                  ),
+                ),
+                SizedBox(height: Spacing.xSmall),
+                Row(
+                  children: [
+                    Text(
+                      t.vault.poweredBy,
+                      style: Typo.xSmall.copyWith(
+                        color: colorScheme.onSecondary,
+                      ),
+                    ),
+                    SizedBox(width: Spacing.superExtraSmall),
+                    Assets.icons.icSafe.svg(),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/core/presentation/pages/vault/vaults_listing_page/views/vaults_list_view.dart
+++ lib/core/presentation/pages/vault/vaults_listing_page/views/vaults_list_view.dart
@@ -1,5 +1,7 @@
+import 'package:app/core/presentation/pages/vault/vaults_listing_page/views/add_vault_bottomsheet.dart';
 import 'package:app/core/presentation/pages/vault/vaults_listing_page/widgets/add_vault_button.dart';
 import 'package:app/core/presentation/pages/vault/vaults_listing_page/widgets/vault_item.dart';
+import 'package:app/core/utils/bottomsheet_utils.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +14,12 @@ class VaultsListView extends StatelessWidget {
       padding: EdgeInsets.all(Spacing.smMedium),
       itemBuilder: (context, index) {
         if (index == 0) {
-          return const AddVaultButton();
+          return AddVaultButton(
+            onPressAdd: () => BottomSheetUtils.showSnapBottomSheet(
+              context,
+              builder: (context) => const AddVaultBottomSheet(),
+            ),
+          );
         }
         return const VaultItem();
       },
