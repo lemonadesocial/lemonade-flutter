0ad2cf22779d0a4ae6fa66d8ef91bf0b3eb19c12
sieuhuflit <sieuhuflit@gmail.com>
2023-12-07 20:43:48 +0700

    feat(chat-ai): audit feature chat ai

--- lib/core/utils/gql/ai_gql_client.dart
+++ lib/core/utils/gql/ai_gql_client.dart
@@ -19,16 +19,25 @@ class AIClient {
 
   Client get client => _client;
 
-  init() async {
-    final appOauth = getIt<AppOauth>();
+  final appOauth = getIt<AppOauth>();
 
+  Future _initializeHive() async {
     await Hive.initFlutter();
-
     final box = await Hive.openBox<Map<String, dynamic>>("graphql");
     await box.clear();
-    final store = ferry_hive_store.HiveStore(box);
-    final cache = Cache(store: store);
+    return box;
+  }
 
+  void _disposeClient() {
+    _authLink.dispose();
+    _webSocketLink.dispose();
+    _client.link.dispose();
+    _client.cache.dispose();
+    _client.cache.clear();
+    _client.cache.clearOptimisticPatches();
+  }
+
+  void _initializeWebSocketLink() {
     _webSocketLink = WebSocketLink(
       AppConfig.wssAIUrl,
       config: SocketClientConfig(
@@ -41,39 +50,59 @@ class AIClient {
       ),
       subProtocol: GraphQLProtocol.graphqlTransportWs,
     );
+  }
 
+  void _initializeAuthLink() {
     _authLink = AuthLink(
       getToken: () async {
         var token = await appOauth.getTokenForGql();
         return 'Bearer $token';
       },
     );
+  }
+
+  void _initializeClient() async {
+    final store = ferry_hive_store.HiveStore(await _initializeHive());
+    final cache = Cache(store: store);
 
     _client = Client(
       link: Link.from(
         [
           _authLink.split(
-            (request) {
-              return request.isSubscription;
-            },
+            (request) => request.isSubscription,
             _webSocketLink,
             _httpLink,
           ),
         ],
       ),
       cache: cache,
-      updateCacheHandlers: {
-        // unrelated to the isolate: update the list of authors in the cache when a new author is created
-      },
+      updateCacheHandlers: {},
     );
+  }
 
-    tokenStateSubscription = appOauth.tokenStateStream.listen((tokenState) {
-      if (tokenState == OAuthTokenState.invalid) {
-        _webSocketLink.dispose();
-        _client.cache.dispose();
-        _client.cache.clear();
-        _client.cache.clearOptimisticPatches();
+  void _initializeTokenStateSubscription() {
+    tokenStateSubscription =
+        appOauth.tokenStateStream.listen((tokenState) async {
+      if (tokenState == OAuthTokenState.valid) {
+        _reinitializeClient();
+      } else if (tokenState == OAuthTokenState.invalid) {
+        _disposeClient();
       }
     });
+  }
+
+  void _reinitializeClient() async {
+    await _initializeHive();
+    _initializeWebSocketLink();
+    _initializeAuthLink();
+    _initializeClient();
+  }
+
+  Future<void> init() async {
+    await _initializeHive();
+    _initializeWebSocketLink();
+    _initializeAuthLink();
+    _initializeClient();
+    _initializeTokenStateSubscription();
   }
 }
