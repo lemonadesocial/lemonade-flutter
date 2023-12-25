b8aeb2b4ee19a1a448b8deb4b01da1b9f368a9f7
Sieu Thai <sieuthai@192.168.31.85>
2023-12-25 15:56:11 +0700

    feat: add get timezone method

--- lib/core/application/event/create_event_bloc/create_event_bloc.dart
+++ lib/core/application/event/create_event_bloc/create_event_bloc.dart
@@ -5,6 +5,7 @@ import 'package:app/core/domain/form/string_formz.dart';
 import 'package:app/graphql/backend/schema.graphql.dart';
 import 'package:app/injection/register_module.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_timezone/flutter_native_timezone.dart';
 import 'package:formz/formz.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
@@ -123,6 +124,7 @@ class CreateEventBloc extends Bloc<CreateEventEvent, CreateEventState> {
       ),
     );
     if (state.isValid) {
+      final timezone = await FlutterNativeTimezone.getLocalTimezone();
       emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
       var input = Input$EventInput(
         title: title.value,
@@ -131,7 +133,7 @@ class CreateEventBloc extends Bloc<CreateEventEvent, CreateEventState> {
         verify: false,
         start: DateTime.parse(event.start.toUtc().toIso8601String()),
         end: DateTime.parse(event.end.toUtc().toIso8601String()),
-        timezone: "Asia/Bangkok",
+        timezone: timezone,
         guest_limit: double.parse(
           state.guestLimit ?? EventConstants.defaultEventGuestLimit,
         ),
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -17,6 +17,7 @@ import firebase_messaging
 import flutter_app_badger
 import flutter_appauth
 import flutter_local_notifications
+import flutter_native_timezone
 import flutter_secure_storage_macos
 import flutter_web_auth_2
 import geolocator_apple
@@ -43,6 +44,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterAppBadgerPlugin.register(with: registry.registrar(forPlugin: "FlutterAppBadgerPlugin"))
   FlutterAppauthPlugin.register(with: registry.registrar(forPlugin: "FlutterAppauthPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FlutterNativeTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterNativeTimezonePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   FlutterWebAuth2Plugin.register(with: registry.registrar(forPlugin: "FlutterWebAuth2Plugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
