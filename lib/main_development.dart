2ba5172fc0ba20de306977030942b4fdcdcec44e
sieuhuflit <sieuhuflit@gmail.com>
2024-02-27 22:52:08 +0700

    fix: lock orientations

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -4,10 +4,12 @@ import 'package:app/core/service/firebase/firebase_service.dart';
 import 'package:app/core/service/matrix/matrix_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:graphql_flutter/graphql_flutter.dart' as gql_flutter;
 import 'package:app/injection/register_module.dart';
+import 'package:app/core/service/wallet/wallet_connect_service.dart';
 
 void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
@@ -21,9 +23,11 @@ void main() async {
     await getIt<FirebaseService>().initialize();
   }
   await getIt<MatrixService>().init();
-
+  await getIt<WalletConnectService>().init();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const LemonadeApp());
-
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const LemonadeApp());
+  });
   debugPrint('App is ready!!! ✅');
 }
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -5,6 +5,7 @@ import 'package:app/core/service/matrix/matrix_service.dart';
 import 'package:app/core/service/wallet/wallet_connect_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:graphql_flutter/graphql_flutter.dart' as gql_flutter;
@@ -22,11 +23,11 @@ void main() async {
     await getIt<FirebaseService>().initialize();
   }
   await getIt<MatrixService>().init();
-
   await getIt<WalletConnectService>().init();
-
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-  runApp(const LemonadeApp());
-
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const LemonadeApp());
+  });
   debugPrint('App is ready!!! ✅');
 }
