a5beab654e814b175f33a9787fc0d8d865e309ef
Sieu Thai <sieuhuflit@gmail.com>
2024-03-04 08:49:07 +0700

    feat: my events (#444)
    ## Overview
    - Added my events screen
    
    ## Demo
    
    
    https://github.com/lemonadesocial/lemonade-flutter/assets/7247063/cec66aca-d069-4aef-b118-6124c824e54e

--- lib/core/presentation/widgets/common/drawer/lemon_drawer.dart
+++ lib/core/presentation/widgets/common/drawer/lemon_drawer.dart
@@ -1,4 +1,3 @@
-import 'package:app/core/domain/event/event_enums.dart';
 import 'package:app/core/presentation/widgets/back_button_widget.dart';
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
 import 'package:app/core/presentation/widgets/common/drawer/widgets/lemon_drawer_profile_info.dart';
@@ -141,11 +140,9 @@ class LemonDrawer extends StatelessWidget {
                     onTap: () {
                       Vibrate.feedback(FeedbackType.light);
                       context.read<AuthBloc>().state.maybeWhen(
-                            authenticated: (authSession) => context.router.push(
-                              EventsListingRoute(
-                                eventListingType: EventListingType.hosting,
-                              ),
-                            ),
+                            authenticated: (authSession) =>
+                                AutoRouter.of(context)
+                                    .navigate(const MyEventsRoute()),
                             orElse: () => context.router.navigate(
                               const LoginRoute(),
                             ),
