c798f06b0dfc77445a45534a8d7a3857f16fb977
Thinh CP <thinh@coderpush.com>
2024-03-04 16:17:26 +0700

    fix: replace ui for setting page if user unauthenticated

--- lib/core/presentation/pages/setting/setting_page.dart
+++ lib/core/presentation/pages/setting/setting_page.dart
@@ -5,6 +5,7 @@ import 'package:app/core/presentation/pages/setting/widgets/setting_profile_tile
 import 'package:app/core/presentation/pages/setting/widgets/setting_tile_widget.dart';
 import 'package:app/core/presentation/widgets/back_button_widget.dart';
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
+import 'package:app/core/presentation/widgets/common/list/empty_list_widget.dart';
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
@@ -82,157 +83,169 @@ class SettingPageView extends StatelessWidget {
         body: SingleChildScrollView(
           child: SafeArea(
             minimum: EdgeInsets.symmetric(horizontal: Spacing.smMedium),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: Spacing.superExtraSmall),
-                const SettingProfileTile(),
-                SizedBox(height: 24.h),
-                SettingTileWidget(
-                  title: t.common.vaults,
-                  subTitle: t.setting.vaultDesc,
-                  leading: ThemeSvgIcon(
-                    color: colorScheme.onPrimary.withOpacity(0.54),
-                    builder: (filter) {
-                      return Assets.icons.icBank.svg(colorFilter: filter);
-                    },
-                  ),
-                  featureAvailable: true,
-                  onTap: () =>
-                      AutoRouter.of(context).push(const VaultRootRoute()),
-                ),
-                SizedBox(height: Spacing.xSmall),
-                SettingTileWidget(
-                  title: t.setting.notification,
-                  subTitle: t.setting.notificationDesc,
-                  leading: Assets.icons.icNotification.svg(),
-                  trailing: Assets.icons.icArrowBack.svg(
-                    width: 18.w,
-                    height: 18.w,
-                  ),
-                  onTap: () =>
-                      context.router.push(const NotificationSettingRoute()),
-                ),
-                SizedBox(height: Spacing.xSmall),
-                SettingTileWidget(
-                  title: t.setting.blockAccount,
-                  subTitle: t.setting.blockAccountDesc,
-                  leading: Assets.icons.icBlock.svg(),
-                  trailing: Assets.icons.icArrowBack.svg(
-                    width: 18.w,
-                    height: 18.w,
-                  ),
-                  onTap: () => context.router.push(const SettingBlockRoute()),
-                ),
-                SizedBox(height: 24.h),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                    vertical: Spacing.xSmall,
-                    horizontal: Spacing.superExtraSmall,
-                  ),
-                  child: Text(
-                    t.setting.about,
-                    style: Typo.medium.copyWith(
-                      color: colorScheme.onPrimary,
-                      fontWeight: FontWeight.w600,
-                    ),
+            child: BlocBuilder<AuthBloc, AuthState>(
+              builder: (context, state) => state.maybeWhen(
+                orElse: () => Center(
+                  child: EmptyList(
+                    emptyText: t.common.somethingWrong,
                   ),
                 ),
-                SettingTileWidget(
-                  title: t.setting.policy,
-                  leading: Assets.icons.icPrivacy.svg(),
-                  trailing: Assets.icons.icExpand.svg(
-                    width: 18.w,
-                    height: 18.w,
-                  ),
-                  onTap: () => ChromeSafariBrowser().open(
-                    url: Uri.parse('https://lemonade.social/privacy'),
-                  ),
-                ),
-                SizedBox(height: Spacing.xSmall),
-                SettingTileWidget(
-                  title: t.setting.term,
-                  leading: Assets.icons.icTerm.svg(),
-                  trailing: Assets.icons.icExpand.svg(
-                    width: 18.w,
-                    height: 18.w,
-                  ),
-                  onTap: () => ChromeSafariBrowser().open(
-                    url: Uri.parse('https://lemonade.social/terms'),
-                  ),
-                ),
-                SizedBox(height: Spacing.xSmall),
-                SettingTileWidget(
-                  title: t.auth.logout,
-                  subTitle: t.setting.logoutDesc,
-                  leading: Assets.icons.icLogout.svg(),
-                  trailing: Assets.icons.icArrowBack.svg(
-                    width: 18.w,
-                    height: 18.w,
-                  ),
-                  onTap: () => context.read<AuthBloc>().add(
-                        const AuthEvent.logout(),
+                authenticated: (_) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: Spacing.superExtraSmall),
+                    const SettingProfileTile(),
+                    SizedBox(height: 24.h),
+                    SettingTileWidget(
+                      title: t.common.vaults,
+                      subTitle: t.setting.vaultDesc,
+                      leading: ThemeSvgIcon(
+                        color: colorScheme.onPrimary.withOpacity(0.54),
+                        builder: (filter) {
+                          return Assets.icons.icBank.svg(colorFilter: filter);
+                        },
                       ),
-                ),
-                SizedBox(height: Spacing.xSmall),
-                SettingTileWidget(
-                  title: t.setting.deleteAccount,
-                  subTitle: t.setting.deleteAccountDesc,
-                  leading: ThemeSvgIcon(
-                    color: LemonColor.deleteAccountRed,
-                    builder: (filter) {
-                      return Assets.icons.icDelete.svg(colorFilter: filter);
-                    },
-                  ),
-                  titleColor: LemonColor.deleteAccountRed,
-                  trailing: Assets.icons.icArrowBack.svg(
-                    width: 18.w,
-                    height: 18.w,
-                  ),
-                  onTap: () async {
-                    final confirmDeleteAccount = await showDialog(
-                      context: context,
-                      barrierDismissible: true,
-                      builder: (BuildContext context) {
-                        return const SettingDeleteAccountDialog();
-                      },
-                    ) as bool;
-                    if (confirmDeleteAccount) {
-                      context
-                          .read<DeleteUserBloc>()
-                          .add(DeleteUserEvent.delete());
-                    }
-                  },
-                ),
-                // Display version information
-                FutureBuilder<String>(
-                  future: getVersion(),
-                  builder:
-                      (BuildContext context, AsyncSnapshot<String> snapshot) {
-                    if (snapshot.hasData) {
-                      return Padding(
-                        padding: EdgeInsets.only(
-                          top: Spacing.large,
-                          bottom: Spacing.xLarge,
+                      featureAvailable: true,
+                      onTap: () =>
+                          AutoRouter.of(context).push(const VaultRootRoute()),
+                    ),
+                    SizedBox(height: Spacing.xSmall),
+                    SettingTileWidget(
+                      title: t.setting.notification,
+                      subTitle: t.setting.notificationDesc,
+                      leading: Assets.icons.icNotification.svg(),
+                      trailing: Assets.icons.icArrowBack.svg(
+                        width: 18.w,
+                        height: 18.w,
+                      ),
+                      onTap: () =>
+                          context.router.push(const NotificationSettingRoute()),
+                    ),
+                    SizedBox(height: Spacing.xSmall),
+                    SettingTileWidget(
+                      title: t.setting.blockAccount,
+                      subTitle: t.setting.blockAccountDesc,
+                      leading: Assets.icons.icBlock.svg(),
+                      trailing: Assets.icons.icArrowBack.svg(
+                        width: 18.w,
+                        height: 18.w,
+                      ),
+                      onTap: () =>
+                          context.router.push(const SettingBlockRoute()),
+                    ),
+                    SizedBox(height: 24.h),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: Spacing.xSmall,
+                        horizontal: Spacing.superExtraSmall,
+                      ),
+                      child: Text(
+                        t.setting.about,
+                        style: Typo.medium.copyWith(
+                          color: colorScheme.onPrimary,
+                          fontWeight: FontWeight.w600,
                         ),
-                        child: Center(
-                          child: Text(
-                            t.common.appVersion(
-                              appVersion: snapshot.data as String,
-                            ),
-                            style: Typo.medium.copyWith(
-                              color: colorScheme.onSurfaceVariant,
-                              fontWeight: FontWeight.w400,
-                            ),
+                      ),
+                    ),
+                    SettingTileWidget(
+                      title: t.setting.policy,
+                      leading: Assets.icons.icPrivacy.svg(),
+                      trailing: Assets.icons.icExpand.svg(
+                        width: 18.w,
+                        height: 18.w,
+                      ),
+                      onTap: () => ChromeSafariBrowser().open(
+                        url: Uri.parse('https://lemonade.social/privacy'),
+                      ),
+                    ),
+                    SizedBox(height: Spacing.xSmall),
+                    SettingTileWidget(
+                      title: t.setting.term,
+                      leading: Assets.icons.icTerm.svg(),
+                      trailing: Assets.icons.icExpand.svg(
+                        width: 18.w,
+                        height: 18.w,
+                      ),
+                      onTap: () => ChromeSafariBrowser().open(
+                        url: Uri.parse('https://lemonade.social/terms'),
+                      ),
+                    ),
+                    SizedBox(height: Spacing.xSmall),
+                    SettingTileWidget(
+                      title: t.auth.logout,
+                      subTitle: t.setting.logoutDesc,
+                      leading: Assets.icons.icLogout.svg(),
+                      trailing: Assets.icons.icArrowBack.svg(
+                        width: 18.w,
+                        height: 18.w,
+                      ),
+                      onTap: () => context.read<AuthBloc>().add(
+                            const AuthEvent.logout(),
                           ),
-                        ),
-                      );
-                    } else {
-                      return const SizedBox.shrink();
-                    }
-                  },
+                    ),
+                    SizedBox(height: Spacing.xSmall),
+                    SettingTileWidget(
+                      title: t.setting.deleteAccount,
+                      subTitle: t.setting.deleteAccountDesc,
+                      leading: ThemeSvgIcon(
+                        color: LemonColor.deleteAccountRed,
+                        builder: (filter) {
+                          return Assets.icons.icDelete.svg(colorFilter: filter);
+                        },
+                      ),
+                      titleColor: LemonColor.deleteAccountRed,
+                      trailing: Assets.icons.icArrowBack.svg(
+                        width: 18.w,
+                        height: 18.w,
+                      ),
+                      onTap: () async {
+                        final confirmDeleteAccount = await showDialog(
+                          context: context,
+                          barrierDismissible: true,
+                          builder: (BuildContext context) {
+                            return const SettingDeleteAccountDialog();
+                          },
+                        ) as bool;
+                        if (confirmDeleteAccount) {
+                          context
+                              .read<DeleteUserBloc>()
+                              .add(DeleteUserEvent.delete());
+                        }
+                      },
+                    ),
+                    // Display version information
+                    FutureBuilder<String>(
+                      future: getVersion(),
+                      builder: (
+                        BuildContext context,
+                        AsyncSnapshot<String> snapshot,
+                      ) {
+                        if (snapshot.hasData) {
+                          return Padding(
+                            padding: EdgeInsets.only(
+                              top: Spacing.large,
+                              bottom: Spacing.xLarge,
+                            ),
+                            child: Center(
+                              child: Text(
+                                t.common.appVersion(
+                                  appVersion: snapshot.data as String,
+                                ),
+                                style: Typo.medium.copyWith(
+                                  color: colorScheme.onSurfaceVariant,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ),
+                          );
+                        } else {
+                          return const SizedBox.shrink();
+                        }
+                      },
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
