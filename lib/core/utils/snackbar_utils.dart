2c032d7c464ed1d3533059d7c718cadba9d329bc
Mai Thinh <thinh@coderpush.com>
2024-03-06 14:29:04 +0700

    chore: revert using built in snackbar due to conflict with navigator (#455)
    # What ?
    - Revert back to use built-in snack bar because flushbar now conflicts
    with navigator system

--- lib/core/utils/snackbar_utils.dart
+++ lib/core/utils/snackbar_utils.dart
@@ -1,6 +1,4 @@
-import 'package:another_flushbar/flushbar.dart';
 import 'package:app/theme/color.dart';
-import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
 import 'package:flutter/material.dart';
 
@@ -9,17 +7,12 @@ final GlobalKey<ScaffoldMessengerState> _rootScaffoldMessengerKey =
 
 class SnackBarUtils {
   static ColorScheme? _colorScheme;
-  static BuildContext? _context;
 
   static GlobalKey<ScaffoldMessengerState> get rootScaffoldMessengerKey =>
       _rootScaffoldMessengerKey;
 
-  static init({
-    required ColorScheme colorScheme,
-    required BuildContext context,
-  }) {
+  static init(ColorScheme colorScheme) {
     _colorScheme = colorScheme;
-    _context = context;
   }
 
   static void showCustomSnackbar(SnackBar snackbar) {
@@ -42,36 +35,30 @@ class SnackBarUtils {
   }
 
   static void showErrorSnackbar(String message) {
-    Flushbar(
-      duration: const Duration(seconds: 2),
-      borderRadius: BorderRadius.circular(LemonRadius.extraSmall),
-      margin: EdgeInsets.symmetric(
-        horizontal: Spacing.smMedium,
+    rootScaffoldMessengerKey.currentState?.showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: Typo.medium
+              .copyWith(color: _colorScheme?.onPrimary ?? LemonColor.white),
+        ),
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: const Color(0xffc0392b),
       ),
-      flushbarPosition: FlushbarPosition.TOP,
-      backgroundColor: const Color(0xffc0392b),
-      messageText: Text(
-        message,
-        style: Typo.medium
-            .copyWith(color: _colorScheme?.onPrimary ?? LemonColor.white),
-      ),
-    ).show(_context!);
+    );
   }
 
   static void showSuccessSnackbar(String message) {
-    Flushbar(
-      duration: const Duration(seconds: 2),
-      borderRadius: BorderRadius.circular(LemonRadius.extraSmall),
-      margin: EdgeInsets.symmetric(
-        horizontal: Spacing.smMedium,
+    rootScaffoldMessengerKey.currentState?.showSnackBar(
+      SnackBar(
+        content: Text(
+          message,
+          style: Typo.medium
+              .copyWith(color: _colorScheme?.onPrimary ?? LemonColor.white),
+        ),
+        behavior: SnackBarBehavior.floating,
+        backgroundColor: LemonColor.snackBarSuccess,
       ),
-      flushbarPosition: FlushbarPosition.TOP,
-      backgroundColor: LemonColor.snackBarSuccess,
-      messageText: Text(
-        message,
-        style: Typo.medium
-            .copyWith(color: _colorScheme?.onPrimary ?? LemonColor.white),
-      ),
-    ).show(_context!);
+    );
   }
 }
