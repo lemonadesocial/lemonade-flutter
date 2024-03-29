f8ed7233dc034b9679fd072009b60482c362905e
Thinh CP <thinh@coderpush.com>
2024-03-29 15:44:40 +0700

    fix: prevent uninvited guests to select tickets on private event

--- lib/core/presentation/pages/event/event_detail_page/guest_event_detail_page/widgets/guest_event_detail_buy_button.dart
+++ lib/core/presentation/pages/event/event_detail_page/guest_event_detail_page/widgets/guest_event_detail_buy_button.dart
@@ -12,13 +12,18 @@ import 'package:app/core/failure.dart';
 import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
 import 'package:app/core/presentation/widgets/future_loading_dialog.dart';
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/core/utils/auth_utils.dart';
 import 'package:app/core/utils/event_tickets_utils.dart';
+import 'package:app/core/utils/event_utils.dart';
 import 'package:app/core/utils/list_utils.dart';
+import 'package:app/core/utils/snackbar_utils.dart';
 import 'package:app/core/utils/string_utils.dart';
 import 'package:app/gen/assets.gen.dart';
 import 'package:app/gen/fonts.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
 import 'package:app/injection/register_module.dart';
 import 'package:app/router/app_router.gr.dart';
+import 'package:app/theme/color.dart';
 import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
@@ -95,8 +100,14 @@ class _GuestEventDetailBuyButtonView extends StatelessWidget {
     return;
   }
 
+  bool _checkInvited(BuildContext context) {
+    final userId = AuthUtils.getUserId(context);
+    return EventUtils.isInvited(event, userId: userId);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final t = Translations.of(context);
     final authState = context.watch<AuthBloc>().state;
     final colorScheme = Theme.of(context).colorScheme;
     final defaultTicketType =
@@ -137,6 +148,35 @@ class _GuestEventDetailBuyButtonView extends StatelessWidget {
                 onTap: () {
                   authState.maybeWhen(
                     authenticated: (_) async {
+                      if (event.private == true) {
+                        if (!_checkInvited(context)) {
+                          return SnackBarUtils.showCustom(
+                            title: t.event.eventPrivate.eventPrivateTitle,
+                            message: t.event.eventPrivate
+                                .eventPrivateErrorDescription,
+                            icon: Container(
+                              width: Sizing.medium,
+                              height: Sizing.medium,
+                              decoration: BoxDecoration(
+                                color: LemonColor.chineseBlack,
+                                borderRadius:
+                                    BorderRadius.circular(Sizing.medium),
+                              ),
+                              child: Center(
+                                child: ThemeSvgIcon(
+                                  color: colorScheme.onSecondary,
+                                  builder: (colorFilter) =>
+                                      Assets.icons.icLock.svg(
+                                    width: Sizing.xSmall,
+                                    height: Sizing.xSmall,
+                                    colorFilter: colorFilter,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                      }
                       final result = await _checkJoinRequest(context);
                       final eventJoinRequest = result?.getOrElse(() => null);
                       if (eventJoinRequest != null) {
