b92ba5c3556a5d050e04b955677573810ef98e5b
Mai Thinh <thinh@coderpush.com>
2024-04-08 10:58:46 +0700

    fix: add follow function to event host list (#521)
    # What ?
    - Add follow function to event host list
    
    # Screenshot
    
    
    https://github.com/lemonadesocial/lemonade-flutter/assets/28992487/8183c85e-0bf2-4442-8fe4-0841b2e4a05d

--- lib/core/presentation/pages/event/event_detail_page/guest_event_detail_page/widgets/guest_event_detail_hosts.dart
+++ lib/core/presentation/pages/event/event_detail_page/guest_event_detail_page/widgets/guest_event_detail_hosts.dart
@@ -1,14 +1,17 @@
 import 'package:app/core/application/auth/auth_bloc.dart';
+import 'package:app/core/application/profile/user_follows_bloc/user_follows_bloc.dart';
 import 'package:app/core/domain/event/entities/event.dart';
 import 'package:app/core/domain/user/entities/user.dart';
+import 'package:app/core/domain/user/user_repository.dart';
 import 'package:app/core/presentation/widgets/image_placeholder_widget.dart';
+import 'package:app/core/presentation/widgets/loading_widget.dart';
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 import 'package:app/core/utils/event_utils.dart';
 import 'package:app/core/utils/image_utils.dart';
-import 'package:app/core/utils/snackbar_utils.dart';
 import 'package:app/core/utils/string_utils.dart';
 import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
+import 'package:app/injection/register_module.dart';
 import 'package:app/router/app_router.gr.dart';
 import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
@@ -102,147 +105,194 @@ class _EventHostItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {
-        AutoRouter.of(context)
-            .navigate(ProfileRoute(userId: host?.userId ?? ''));
-      },
-      child: Container(
-        width: 130.w,
-        decoration: ShapeDecoration(
-          color: colorScheme.onPrimary.withOpacity(0.06),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(15.r),
-          ),
+    return BlocProvider(
+      create: (context) => UserFollowsBloc(
+        getIt<UserRepository>(),
+      )..add(
+          UserFollowsEvent.fetch(followee: host?.userId ?? ''),
         ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(15.r),
-          child: Stack(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: Spacing.small,
-                  horizontal: Spacing.extraSmall,
-                ),
-                child: Center(
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(60.w),
-                        ),
-                        width: 60.w,
-                        height: 60.w,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(60.r),
-                          child: CachedNetworkImage(
-                            fit: BoxFit.cover,
-                            placeholder: (_, __) =>
-                                ImagePlaceholder.defaultPlaceholder(),
-                            errorWidget: (_, __, ___) =>
-                                ImagePlaceholder.defaultPlaceholder(),
-                            imageUrl: ImageUtils.generateUrl(
-                              file: host?.newPhotosExpanded?.isNotEmpty == true
-                                  ? host?.newPhotosExpanded?.first
-                                  : null,
-                              imageConfig: ImageConfig.eventPhoto,
+      child: InkWell(
+        onTap: () {
+          AutoRouter.of(context)
+              .navigate(ProfileRoute(userId: host?.userId ?? ''));
+        },
+        child: Container(
+          width: 130.w,
+          decoration: ShapeDecoration(
+            color: colorScheme.onPrimary.withOpacity(0.06),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15.r),
+            ),
+          ),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(15.r),
+            child: Stack(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: Spacing.small,
+                    horizontal: Spacing.extraSmall,
+                  ),
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(60.w),
+                          ),
+                          width: 60.w,
+                          height: 60.w,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(60.r),
+                            child: CachedNetworkImage(
+                              fit: BoxFit.cover,
+                              placeholder: (_, __) =>
+                                  ImagePlaceholder.defaultPlaceholder(),
+                              errorWidget: (_, __, ___) =>
+                                  ImagePlaceholder.defaultPlaceholder(),
+                              imageUrl: ImageUtils.generateUrl(
+                                file:
+                                    host?.newPhotosExpanded?.isNotEmpty == true
+                                        ? host?.newPhotosExpanded?.first
+                                        : null,
+                                imageConfig: ImageConfig.eventPhoto,
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(height: Spacing.xSmall),
-                      Text(
-                        host?.displayName ?? host?.name ?? '',
-                        style: Typo.small.copyWith(
-                          color: colorScheme.onPrimary,
-                        ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      Text(
-                        host?.jobTitle ?? '',
-                        style: Typo.small.copyWith(
-                          color: colorScheme.onSecondary,
-                        ),
-                        maxLines: 1,
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                      if (isAttending) ...[
                         SizedBox(height: Spacing.xSmall),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              height: 30.w,
-                              padding: EdgeInsets.symmetric(
-                                horizontal: Spacing.xSmall,
-                              ),
-                              decoration: ShapeDecoration(
-                                color: colorScheme.onPrimary.withOpacity(0.06),
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(
-                                    LemonRadius.xSmall,
-                                  ),
-                                ),
-                              ),
-                              child: InkWell(
-                                onTap: () {
-                                  SnackBarUtils.showComingSoon();
-                                },
-                                child: Center(
-                                  child: Text(
-                                    t.common.actions.follow,
-                                    style: Typo.small.copyWith(
-                                      color: colorScheme.onSecondary,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: Spacing.superExtraSmall),
-                            InkWell(
-                              onTap: () {
-                                AutoRouter.of(context)
-                                    .navigate(const ChatListRoute());
-                              },
-                              child: Container(
-                                height: 30.w,
-                                padding: EdgeInsets.symmetric(
-                                  horizontal: Spacing.superExtraSmall,
-                                ),
-                                decoration: ShapeDecoration(
-                                  color:
-                                      colorScheme.onPrimary.withOpacity(0.06),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(
-                                      LemonRadius.xSmall,
-                                    ),
-                                  ),
-                                ),
-                                child: Center(
-                                  child: ThemeSvgIcon(
-                                    color: colorScheme.onSecondary,
-                                    builder: (filter) =>
-                                        Assets.icons.icChatBubble.svg(
-                                      colorFilter: filter,
-                                      width: Sizing.xSmall,
-                                      height: Sizing.xSmall,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                          ],
+                        Text(
+                          host?.displayName ?? host?.name ?? '',
+                          style: Typo.small.copyWith(
+                            color: colorScheme.onPrimary,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
                         ),
+                        Text(
+                          host?.jobTitle ?? '',
+                          style: Typo.small.copyWith(
+                            color: colorScheme.onSecondary,
+                          ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                        if (isAttending) ...[
+                          SizedBox(height: Spacing.xSmall),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              _FollowButton(host: host),
+                              SizedBox(width: Spacing.superExtraSmall),
+                              InkWell(
+                                onTap: () {
+                                  AutoRouter.of(context)
+                                      .navigate(const ChatListRoute());
+                                },
+                                child: Container(
+                                  height: 30.w,
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: Spacing.superExtraSmall,
+                                  ),
+                                  decoration: ShapeDecoration(
+                                    color:
+                                        colorScheme.onPrimary.withOpacity(0.06),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(
+                                        LemonRadius.xSmall,
+                                      ),
+                                    ),
+                                  ),
+                                  child: Center(
+                                    child: ThemeSvgIcon(
+                                      color: colorScheme.onSecondary,
+                                      builder: (filter) =>
+                                          Assets.icons.icChatBubble.svg(
+                                        colorFilter: filter,
+                                        width: Sizing.xSmall,
+                                        height: Sizing.xSmall,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ],
-                    ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _FollowButton extends StatelessWidget {
+  final User? host;
+  const _FollowButton({
+    this.host,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      height: 30.w,
+      padding: EdgeInsets.symmetric(
+        horizontal: Spacing.xSmall,
+      ),
+      decoration: ShapeDecoration(
+        color: colorScheme.onPrimary.withOpacity(0.06),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(
+            LemonRadius.xSmall,
+          ),
+        ),
+      ),
+      child: BlocBuilder<UserFollowsBloc, UserFollowsState>(
+        builder: (context, state) {
+          final following = state.maybeWhen(
+            orElse: () => false,
+            fetched: (userFollows) => userFollows.isNotEmpty,
+          );
+          final isLoading = state.maybeWhen(
+            orElse: () => false,
+            loading: () => true,
+          );
+
+          return InkWell(
+            onTap: () {
+              if (isLoading) {
+                return;
+              }
+              if (following) {
+                context.read<UserFollowsBloc>().add(
+                      UserFollowsEvent.unfollow(followee: host?.userId ?? ''),
+                    );
+              } else {
+                context.read<UserFollowsBloc>().add(
+                      UserFollowsEvent.follow(followee: host?.userId ?? ''),
+                    );
+              }
+            },
+            child: Center(
+              child: isLoading
+                  ? Loading.defaultLoading(context)
+                  : Text(
+                      following ? t.common.followed : t.common.actions.follow,
+                      style: Typo.small.copyWith(
+                        color: colorScheme.onSecondary,
+                      ),
+                    ),
+            ),
+          );
+        },
       ),
     );
   }
