a1aa9dc559b10ddb3cc8925aa834df2e04369e82
Mai Thinh <thinh@coderpush.com>
2024-04-15 16:18:31 +0700

    fix: fix display and search function in event cohost settings (#543)
    # What ?
    - Fix display pending cohosts request
    - Fix error searching user when adding cohosts
    <img width="447" alt="Screenshot 2024-04-15 at 16 15 08"
    src="https://github.com/lemonadesocial/lemonade-flutter/assets/28992487/daa370b9-04bb-41a3-8129-586e6ac53469">

--- lib/core/domain/event/entities/event_cohost_request.dart
+++ lib/core/domain/event/entities/event_cohost_request.dart
@@ -1,19 +1,23 @@
 import 'package:app/core/data/event/dtos/event_cohost_request_dto/event_cohost_request_dto.dart';
+import 'package:app/core/domain/event/event_enums.dart';
 import 'package:app/core/domain/user/entities/user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-class EventCohostRequest {
-  EventCohostRequest({
-    this.id,
-    this.toExpanded,
-  });
+part 'event_cohost_request.freezed.dart';
 
-  String? id;
-  User? toExpanded;
+@freezed
+class EventCohostRequest with _$EventCohostRequest {
+  factory EventCohostRequest({
+    String? id,
+    User? toExpanded,
+    EventCohostRequestState? state,
+  }) = _EventCohostRequest;
 
   factory EventCohostRequest.fromDto(EventCohostRequestDto dto) =>
       EventCohostRequest(
         id: dto.id,
         toExpanded:
             dto.toExpanded != null ? User.fromDto(dto.toExpanded!) : null,
+        state: dto.state,
       );
 }
