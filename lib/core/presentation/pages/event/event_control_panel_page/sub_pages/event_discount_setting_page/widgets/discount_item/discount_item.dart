2ebfa5e1dd221737f54c00c1f0153ef7fa315d2e
sieuhuflit <sieuhuflit@gmail.com>
2024-04-17 09:38:28 +0700

    chore: resolve comment

--- lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_discount_setting_page/widgets/discount_item/discount_item.dart
+++ lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_discount_setting_page/widgets/discount_item/discount_item.dart
@@ -2,6 +2,7 @@ import 'package:app/core/domain/event/entities/event_payment_ticket_discount.dar
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
+import 'package:app/router/app_router.gr.dart';
 import 'package:app/theme/color.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
