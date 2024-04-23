73589131c6ec578d5abc92c4c1633ef737243bea
sieuhuflit <sieuhuflit@gmail.com>
2024-04-23 09:59:25 +0700

    chore: clean up

--- lib/core/service/guild/guild_service.dart
+++ lib/core/service/guild/guild_service.dart
@@ -48,31 +48,4 @@ class GuildService {
       return [];
     }
   }
-
-  static Future<GuildBasic> createGuild({
-    required String name,
-    required List<GuildRole> roles,
-  }) async {
-    final endpoint = '$guildApiEndpoint/v1/guild';
-    final response = await http.post(
-      Uri.parse(endpoint),
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(
-        {
-          "payload": {
-            "name": name,
-            "roles": jsonEncode(jsonDecode(roles.toString())),
-          },
-          "validation": {
-            "address": "0x94228f99a09a30c9f7abfd8b1a629448bf6b0625",
-          },
-        },
-      ),
-    );
-    return GuildBasic.fromJson(
-      jsonDecode(response.body),
-    );
-  }
 }
