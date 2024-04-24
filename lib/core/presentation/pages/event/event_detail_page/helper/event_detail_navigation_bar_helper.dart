97e814b387f7458b4be2057a7345c5f909f8e15a
sieuhuflit <sieuhuflit@gmail.com>
2024-04-24 10:41:22 +0700

    feat: hide some items in event detail navigation bar

--- lib/core/presentation/pages/event/event_detail_page/helper/event_detail_navigation_bar_helper.dart
+++ lib/core/presentation/pages/event/event_detail_page/helper/event_detail_navigation_bar_helper.dart
@@ -1,7 +1,6 @@
 import 'package:app/core/domain/event/entities/event.dart';
 import 'package:app/core/presentation/pages/event/my_event_ticket_page/widgets/ticket_qr_code_popup.dart';
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
-import 'package:app/core/utils/snackbar_utils.dart';
 import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/router/app_router.gr.dart';
