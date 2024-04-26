0b09fb4da0c89d3f4f32082484e8ad882c5a0d81
sieuhuflit <sieuhuflit@gmail.com>
2024-04-26 09:14:41 +0700

    feat: add discover social grid section

--- lib/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_social_grid_section.dart
+++ lib/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_social_grid_section.dart
@@ -1,4 +1,18 @@
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class _ItemData {
+  final String title;
+  final ThemeSvgIcon themeSvgIcon;
+
+  _ItemData({required this.title, required this.themeSvgIcon});
+}
 
 class CollaboratorDiscoverSocialGridSection extends StatelessWidget {
   const CollaboratorDiscoverSocialGridSection({
@@ -6,6 +20,34 @@ class CollaboratorDiscoverSocialGridSection extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final t = Translations.of(context);
+    final List<_ItemData> items = [
+      _ItemData(
+        title: t.profile.socials.farcaster,
+        themeSvgIcon: ThemeSvgIcon(
+          color: colorScheme.onPrimary,
+          builder: (colorFilter) => Assets.icons.icFarcaster.svg(
+            colorFilter: colorFilter,
+            width: 18.w,
+            height: 18.w,
+          ),
+        ),
+      ),
+      _ItemData(
+        title: t.profile.socials.twitter,
+        themeSvgIcon: ThemeSvgIcon(
+          color: colorScheme.onPrimary,
+          builder: (colorFilter) => Assets.icons.icTwitter.svg(
+            colorFilter: colorFilter,
+            width: 18.w,
+            height: 18.w,
+          ),
+        ),
+      ),
+      // Add more items here as needed
+    ];
+
     return SliverGrid(
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         crossAxisCount: 2,
@@ -15,9 +57,9 @@ class CollaboratorDiscoverSocialGridSection extends StatelessWidget {
       ),
       delegate: SliverChildBuilderDelegate(
         (context, index) {
-          return CustomGridItem(
-            title: index == 0 ? 'Farcaster' : 'Twitter',
-            iconUrl: 'https://via.placeholder.com/10x9', // Placeholder URL
+          return _Item(
+            title: items[index].title,
+            icon: items[index].themeSvgIcon,
           );
         },
         childCount: 2,
@@ -26,80 +68,52 @@ class CollaboratorDiscoverSocialGridSection extends StatelessWidget {
   }
 }
 
-class CustomGridItem extends StatelessWidget {
+class _Item extends StatelessWidget {
   final String title;
-  final String iconUrl;
+  final ThemeSvgIcon icon;
 
-  const CustomGridItem({
-    super.key,
+  const _Item({
     required this.title,
-    required this.iconUrl,
+    required this.icon,
   });
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
     return Container(
       width: double.infinity,
-      height: 54,
-      padding: const EdgeInsets.all(18),
+      padding: EdgeInsets.all(Spacing.smMedium),
       clipBehavior: Clip.antiAlias,
       decoration: ShapeDecoration(
-        color: Colors.white.withOpacity(0.05999999865889549),
+        color: LemonColor.white06,
         shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(18),
+          borderRadius: BorderRadius.circular(LemonRadius.normal),
         ),
-        shadows: const [
-          BoxShadow(
-            color: Color(0x1E000000),
-            blurRadius: 6,
-            offset: Offset(0, 2),
-            spreadRadius: 0,
-          ),
-        ],
       ),
       child: Row(
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            width: 18,
-            height: 18,
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(
-              color: Colors.blue, // Placeholder for icon background color
-            ),
-            child: const Icon(
-              Icons.account_circle, // Placeholder for icon
-              color: Colors.white, // Placeholder for icon color
-            ),
-          ),
-          const SizedBox(width: 9),
+          icon,
+          SizedBox(width: Spacing.extraSmall),
           Expanded(
             child: SizedBox(
               child: Text(
                 title,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
-                  fontFamily: 'Switzer Variable',
+                style: Typo.medium.copyWith(
+                  color: colorScheme.onPrimary,
                   fontWeight: FontWeight.w600,
-                  height: 0,
                 ),
               ),
             ),
           ),
-          const SizedBox(width: 9),
-          Container(
-            width: 12,
-            height: 12,
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(
-              color: Colors.blue, // Placeholder for icon background color
-            ),
-            child: const Icon(
-              Icons.account_circle, // Placeholder for icon
-              color: Colors.white, // Placeholder for icon color
+          SizedBox(width: Spacing.extraSmall),
+          ThemeSvgIcon(
+            builder: (colorFilter) => Assets.icons.icExpand.svg(
+              colorFilter: colorFilter,
+              width: 18.w,
+              height: 18.w,
             ),
           ),
         ],
