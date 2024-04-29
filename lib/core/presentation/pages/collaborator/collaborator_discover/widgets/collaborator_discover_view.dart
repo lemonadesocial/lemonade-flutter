ac691e4a8b00966d781f3c31d181304290dacb4e
sieuhuflit <sieuhuflit@gmail.com>
2024-04-29 13:48:36 +0700

    chore: add missing commented code

--- lib/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_view.dart
+++ lib/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_view.dart
@@ -1,6 +1,7 @@
 import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_activity_section.dart';
 import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_basic_info_section.dart';
 import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_expertise_offering_card.dart';
+import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_icebreakers_section.dart';
 import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_photos_card.dart';
 import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_social_grid_section.dart';
 import 'package:app/core/presentation/pages/collaborator/collaborator_discover/widgets/collaborator_discover_spotlight_events_section.dart';
@@ -58,12 +59,12 @@ class CollaboratorDiscoverView extends StatelessWidget {
           ),
         ),
         const CollaboratorDiscoverSpotlightEventsSection(),
-        // SliverToBoxAdapter(
-        //   child: SizedBox(
-        //     height: 30.w,
-        //   ),
-        // ),
-        // const CollaboratorDiscoverIcebreakersSection(),
+        SliverToBoxAdapter(
+          child: SizedBox(
+            height: 30.w,
+          ),
+        ),
+        const CollaboratorDiscoverIcebreakersSection(),
       ],
     );
   }
