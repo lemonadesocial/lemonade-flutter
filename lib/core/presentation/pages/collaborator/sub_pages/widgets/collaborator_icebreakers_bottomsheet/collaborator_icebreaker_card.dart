cc47b2e0071f18724d7e8ac5954dbc3c6f3096d2
sieuhuflit <sieuhuflit@gmail.com>
2024-05-03 10:22:49 +0700

    feat: implement icebreakers ui flow

---
+++ lib/core/presentation/pages/collaborator/sub_pages/widgets/collaborator_icebreakers_bottomsheet/collaborator_icebreaker_card.dart
@@ -0,0 +1,68 @@
+import 'package:app/core/presentation/pages/collaborator/sub_pages/widgets/collaborator_icebreakers_bottomsheet/collaborator_icebreakers_bottomsheet.dart';
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/gen/fonts.gen.dart';
+import 'package:app/i18n/i18n.g.dart';
+import 'package:app/theme/color.dart';
+import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
+
+class CollaboratorIceBreakerCard extends StatelessWidget {
+  const CollaboratorIceBreakerCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Container(
+      padding: EdgeInsets.all(Spacing.smMedium),
+      clipBehavior: Clip.antiAlias,
+      decoration: ShapeDecoration(
+        color: LemonColor.white06,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(LemonRadius.normal),
+        ),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ThemeSvgIcon(
+                builder: (colorFilter) => Assets.icons.icQuote.svg(
+                  width: 18.w,
+                  height: 18.w,
+                ),
+              ),
+              SizedBox(width: Spacing.smMedium / 2),
+              Expanded(
+                child: Text(
+                  'My most unusual traits are...',
+                  style: Typo.medium.copyWith(
+                    color: colorScheme.onSecondary,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: Spacing.smMedium / 2),
+          Text(
+            'Dad jokes, eye color mismatch, constant fidgeting when happy',
+            style: Typo.mediumPlus.copyWith(
+              color: colorScheme.onPrimary,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
