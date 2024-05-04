bb33adf7b2fa74b5ae6f2be81eb0f6418c8afe50
sieuhuflit <sieuhuflit@gmail.com>
2024-05-05 01:15:11 +0700

    feat: handle social data

--- lib/core/presentation/pages/collaborator/sub_pages/collaborator_edit_profile_page/widgets/collaborator_edit_social_connect/collaborator_edit_social_connect.dart
+++ lib/core/presentation/pages/collaborator/sub_pages/collaborator_edit_profile_page/widgets/collaborator_edit_social_connect/collaborator_edit_social_connect.dart
@@ -1,3 +1,5 @@
+import 'package:app/core/application/auth/auth_bloc.dart';
+import 'package:app/core/domain/user/entities/user.dart';
 import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
 import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 import 'package:app/gen/assets.gen.dart';
@@ -7,13 +9,21 @@ import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:slang/builder/utils/string_extensions.dart';
 
 class CollaboratorEditSocialConnect extends StatelessWidget {
   const CollaboratorEditSocialConnect({super.key});
 
   @override
   Widget build(BuildContext context) {
+    User? user = context.watch<AuthBloc>().state.maybeWhen(
+          authenticated: (authSession) => authSession,
+          orElse: () => null,
+        );
+    final isConnectedFarCaster = user?.handleFarcaster?.isNotEmpty == true;
+    final isConnectedTwitter = user?.handleTwitter?.isNotEmpty == true;
     final t = Translations.of(context);
     final colorScheme = Theme.of(context).colorScheme;
     return SliverToBoxAdapter(
@@ -21,22 +31,30 @@ class CollaboratorEditSocialConnect extends StatelessWidget {
         children: [
           _SocialItem(
             socialTitle: t.profile.socials.farcaster,
+            socialValue: user?.handleFarcaster ?? '',
             socialIcon: ThemeSvgIcon(
               color: colorScheme.onSecondary,
               builder: (filter) => Assets.icons.icFarcaster.svg(
                 colorFilter: filter,
               ),
             ),
+            isConnected: isConnectedFarCaster,
+            isSpecialRadiusTop: true,
+            isSpecialRadiusBottom: false,
           ),
           SizedBox(height: Spacing.superExtraSmall),
           _SocialItem(
             socialTitle: t.profile.socials.twitter,
+            socialValue: user?.handleTwitter ?? '',
             socialIcon: ThemeSvgIcon(
               color: colorScheme.onSecondary,
               builder: (filter) => Assets.icons.icXLine.svg(
                 colorFilter: filter,
               ),
             ),
+            isConnected: isConnectedTwitter,
+            isSpecialRadiusTop: false,
+            isSpecialRadiusBottom: true,
           ),
         ],
       ),
@@ -46,20 +64,46 @@ class CollaboratorEditSocialConnect extends StatelessWidget {
 
 class _SocialItem extends StatelessWidget {
   final String socialTitle;
+  final String socialValue;
   final Widget socialIcon;
+  final bool isConnected;
+  final bool? isSpecialRadiusTop;
+  final bool? isSpecialRadiusBottom;
   const _SocialItem({
     required this.socialTitle,
+    required this.socialValue,
     required this.socialIcon,
+    required this.isConnected,
+    this.isSpecialRadiusBottom,
+    this.isSpecialRadiusTop,
   });
 
   @override
   Widget build(BuildContext context) {
     final t = Translations.of(context);
     final colorScheme = Theme.of(context).colorScheme;
+    final lineColor =
+        isConnected == true ? LemonColor.snackBarSuccess : LemonColor.coralReef;
+    final connectInfo = isConnected == true
+        ? '${t.common.status.connected} : $socialValue'
+        : t.common.status.notConnected;
+
+    final double topRadius = isSpecialRadiusTop == true
+        ? LemonRadius.medium
+        : LemonRadius.extraSmall;
+    final double bottomRadius = isSpecialRadiusBottom == true
+        ? LemonRadius.medium
+        : LemonRadius.extraSmall;
+
     return Container(
       padding: EdgeInsets.all(Spacing.small),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(LemonRadius.extraSmall),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(topRadius),
+          topRight: Radius.circular(topRadius),
+          bottomLeft: Radius.circular(bottomRadius),
+          bottomRight: Radius.circular(bottomRadius),
+        ),
         color: LemonColor.atomicBlack,
       ),
       child: Row(
@@ -85,26 +129,28 @@ class _SocialItem extends StatelessWidget {
                   style: Typo.medium.copyWith(
                     color: colorScheme.onPrimary,
                     fontWeight: FontWeight.w600,
+                    height: 0,
                   ),
                 ),
                 SizedBox(height: 2.w),
                 Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   mainAxisSize: MainAxisSize.min,
                   children: [
                     Container(
                       width: 2.w,
                       height: 9.w,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(2.w),
-                        color: LemonColor.coralReef,
+                        borderRadius: BorderRadius.circular(1.w),
+                        color: lineColor,
                       ),
                     ),
-                    SizedBox(width: 3.w),
+                    SizedBox(width: 5.w),
                     Text(
-                      t.common.status.notConnected,
+                      connectInfo,
                       style: Typo.small.copyWith(
                         color: colorScheme.onSecondary,
+                        height: 0,
                       ),
                     ),
                   ],
@@ -112,13 +158,15 @@ class _SocialItem extends StatelessWidget {
               ],
             ),
           ),
-          LinearGradientButton.secondaryButton(
-            height: Sizing.medium,
-            label: t.common.actions.connect,
-            textStyle: Typo.small.copyWith(
-              color: colorScheme.onPrimary,
-            ),
-          ),
+          isConnected == false
+              ? LinearGradientButton.secondaryButton(
+                  height: Sizing.medium,
+                  label: t.common.actions.connect.capitalize(),
+                  textStyle: Typo.small.copyWith(
+                    color: colorScheme.onPrimary,
+                  ),
+                )
+              : const SizedBox(),
         ],
       ),
     );
