4ed7e5a30481acb9cddae37a08c67a335cbb9fe6
Sieu Thai <sieuhuflit@gmail.com>
2024-05-31 14:01:07 +0700

    feat: bring back shorebird (#620)
    ## Overview
    
    - Setup shorebird

---
+++ scripts/set_build_number.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if a build number is provided as an argument
+if [ -z "$1" ]; then
+  echo "Usage: $0 <BUILD_NUMBER>"
+  exit 1
+fi
+
+BUILD_NUMBER=$1
+
+# Auto append build number into version of pubspec file
+# Example: 1.20.0 become 1.20.0+35
+perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+)(\+\d+)?$/$1+'$BUILD_NUMBER'/' pubspec.yaml
