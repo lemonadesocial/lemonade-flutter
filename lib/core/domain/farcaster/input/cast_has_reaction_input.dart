6276ae37da37115e37fe370fed4391b7fae0e128
Mai Thinh <thinh@coderpush.com>
2024-06-10 12:23:13 +0700

    feat: implement recast a cast (#632)
    # What ?
    - Implement recast a cast 
    
    # Screenshot 
    
    
    https://github.com/lemonadesocial/lemonade-flutter/assets/28992487/3a1fdcfe-907b-44ea-bdff-92272ee4e7c9

--- lib/core/domain/farcaster/input/cast_has_reaction_input.dart
+++ lib/core/domain/farcaster/input/cast_has_reaction_input.dart
@@ -3,6 +3,13 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'cast_has_reaction_input.freezed.dart';
 part 'cast_has_reaction_input.g.dart';
 
+enum CheckHasReactionType {
+  @JsonValue('REACTION_TYPE_LIKE')
+  like,
+  @JsonValue('REACTION_TYPE_RECAST')
+  recast,
+}
+
 @freezed
 class CastHasReactionInput with _$CastHasReactionInput {
   factory CastHasReactionInput({
@@ -10,8 +17,8 @@ class CastHasReactionInput with _$CastHasReactionInput {
     @JsonKey(name: 'target_fid') required int targetFid,
     @JsonKey(name: 'target_hash') required String hash,
     @JsonKey(name: 'reaction_type')
-    @Default('REACTION_TYPE_LIKE')
-    String? reactionType,
+    @Default(CheckHasReactionType.like)
+    CheckHasReactionType? reactionType,
   }) = _CastHasReactionInput;
 
   factory CastHasReactionInput.fromJson(Map<String, dynamic> json) =>
