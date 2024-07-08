74e7ddc91a84db2a6725676ac06999e6664df69a
sieuhuflit <sieuhuflit@gmail.com>
2024-07-08 11:05:55 +0700

    feat: implement mock quests list

--- lib/core/presentation/pages/quest/quest_listing_page/views/quest_listing_page_view.dart
+++ lib/core/presentation/pages/quest/quest_listing_page/views/quest_listing_page_view.dart
@@ -1,10 +1,13 @@
+import 'package:app/core/presentation/pages/quest/widgets/quest_item_widget.dart';
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
+import 'package:app/core/presentation/widgets/common/list/empty_list_widget.dart';
 import 'package:app/core/utils/string_utils.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/theme/color.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
 import 'package:flutter/material.dart';
+import 'package:matrix/matrix.dart';
 
 enum QuestListingTabs {
   profile(tabIndex: 0),
@@ -81,21 +84,64 @@ class _QuestListingPageViewState extends State<QuestListingPageView>
             Expanded(
               child: TabBarView(
                 controller: _tabController,
-                children: const [
-                  CustomScrollView(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: Spacing.xSmall,
+                      horizontal: Spacing.xSmall,
+                    ),
+                    child: CustomScrollView(
+                      slivers: [
+                        _GuestListSection(
+                          rooms: const [{}, {}, {}, {}, {}, {}],
+                          itemBuilder: () => QuestItemWidget(
+                            title: '1 point',
+                            subTitle: 'Verify email',
+                            onTap: () {},
+                            repeatable: true,
+                          ),
+                          emptyText: t.chat.emptyChannels,
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CustomScrollView(
                     slivers: [],
                   ),
-                  CustomScrollView(
-                    slivers: [],
-                  ),
-                  CustomScrollView(slivers: []),
-                  CustomScrollView(slivers: []),
+                  const CustomScrollView(slivers: []),
+                  const CustomScrollView(slivers: []),
                 ],
               ),
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _GuestListSection extends StatelessWidget {
+  const _GuestListSection({
+    required this.rooms,
+    required this.itemBuilder,
+    this.emptyText,
+  });
+
+  final List<Object> rooms;
+  final Widget Function() itemBuilder;
+  final String? emptyText;
+
+  @override
+  Widget build(BuildContext context) {
+    if (rooms.isEmpty) {
+      return SliverToBoxAdapter(
+        child: EmptyList(emptyText: emptyText),
+      );
+    }
+    return SliverList.separated(
+      itemCount: 6,
+      itemBuilder: (context, index) => itemBuilder(),
+      separatorBuilder: (context, index) => SizedBox(height: Spacing.xSmall),
     );
   }
 }
