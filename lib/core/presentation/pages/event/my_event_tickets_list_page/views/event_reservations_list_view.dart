e09394d204dd0c37b2169eed5a9c3b1c1ee749b6
Mai Thinh <thinh@coderpush.com>
2024-07-16 11:03:00 +0700

    fix: pagination for for tickets list page (event reservation) (#675)
    # What ?
    - Add pagination for tickets list page (event reservation)

--- lib/core/presentation/pages/event/my_event_tickets_list_page/views/event_reservations_list_view.dart
+++ lib/core/presentation/pages/event/my_event_tickets_list_page/views/event_reservations_list_view.dart
@@ -1,17 +1,25 @@
 import 'package:app/core/domain/event/entities/event.dart';
 import 'package:app/core/presentation/pages/event/my_event_tickets_list_page/widgets/event_reservations_list_item_widget.dart';
 import 'package:app/core/presentation/widgets/common/list/empty_list_widget.dart';
+import 'package:app/core/presentation/widgets/loading_widget.dart';
+import 'package:app/core/utils/debouncer.dart';
 import 'package:app/router/app_router.gr.dart';
+import 'package:app/theme/spacing.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class EventReservationsListView extends StatelessWidget {
   final List<Event> eventsList;
+  final Function()? onFetchMore;
+  final bool hasNextPage;
+  final Debouncer debouncer = Debouncer(milliseconds: 300);
 
-  const EventReservationsListView({
+  EventReservationsListView({
     super.key,
     required this.eventsList,
+    this.onFetchMore,
+    this.hasNextPage = true,
   });
 
   @override
@@ -29,26 +37,44 @@ class EventReservationsListView extends StatelessWidget {
     }
 
     return Expanded(
-      child: ListView.separated(
-        itemBuilder: (context, index) {
-          return InkWell(
-            onTap: () {
-              AutoRouter.of(context).navigate(
-                MyEventTicketRoute(
-                  event: eventsList[index],
-                ),
-              );
-            },
-            child: EventReservationsListItemWidget(
-              event: eventsList[index],
-            ),
-          );
+      child: NotificationListener<ScrollNotification>(
+        onNotification: (scrollNotification) {
+          if (scrollNotification.metrics.pixels ==
+              scrollNotification.metrics.maxScrollExtent) {
+            debouncer.run(() => onFetchMore?.call());
+          }
+          return true;
         },
-        itemCount: eventsList.length,
-        separatorBuilder: (context, index) => Divider(
-          color: colorScheme.outline,
-          height: 1.w,
-          thickness: 1.w,
+        child: ListView.separated(
+          itemBuilder: (context, index) {
+            if (index == eventsList.length) {
+              return hasNextPage && eventsList.length > 20
+                  ? Padding(
+                      padding: EdgeInsets.symmetric(vertical: Spacing.smMedium),
+                      child: Loading.defaultLoading(context),
+                    )
+                  : const SizedBox();
+            }
+            return InkWell(
+              key: ValueKey(eventsList[index].id ?? ''),
+              onTap: () {
+                AutoRouter.of(context).navigate(
+                  MyEventTicketRoute(
+                    event: eventsList[index],
+                  ),
+                );
+              },
+              child: EventReservationsListItemWidget(
+                event: eventsList[index],
+              ),
+            );
+          },
+          itemCount: eventsList.length + 1,
+          separatorBuilder: (context, index) => Divider(
+            color: colorScheme.outline,
+            height: 1.w,
+            thickness: 1.w,
+          ),
         ),
       ),
     );
