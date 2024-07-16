a540d5e59288d5f0947f8f422d469ff4e48ddbf0
Thinh CP <thinh@coderpush.com>
2024-07-16 10:52:04 +0700

    fix: pagination for for tickets list page (event reservation)

--- lib/core/presentation/pages/event/my_event_tickets_list_page/my_event_tickets_list_page.dart
+++ lib/core/presentation/pages/event/my_event_tickets_list_page/my_event_tickets_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:app/core/application/event/events_listing_bloc/base_events_listing_bloc.dart';
 import 'package:app/core/application/event/events_listing_bloc/past_events_listing_bloc.dart';
 import 'package:app/core/application/event/events_listing_bloc/upcoming_events_listing_bloc.dart';
+import 'package:app/core/domain/event/entities/event.dart';
 import 'package:app/core/domain/event/event_repository.dart';
 import 'package:app/core/domain/event/input/get_events_listing_input.dart';
 import 'package:app/core/presentation/pages/event/my_event_tickets_list_page/views/event_reservations_list_view.dart';
@@ -11,6 +12,8 @@ import 'package:app/core/presentation/widgets/loading_widget.dart';
 import 'package:app/core/service/event/event_service.dart';
 import 'package:app/core/utils/auth_utils.dart';
 import 'package:app/core/utils/string_utils.dart';
+import 'package:app/graphql/backend/event/query/get_past_events.graphql.dart';
+import 'package:app/graphql/backend/event/query/get_upcoming_events.graphql.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/injection/register_module.dart';
 import 'package:app/theme/spacing.dart';
@@ -70,11 +73,14 @@ class MyEventTicketsListView extends StatefulWidget {
 
 class _MyEventTicketsListViewState extends State<MyEventTicketsListView> {
   ReservationType selectedReservationType = ReservationType.upcoming;
+  bool upcomingEventsHasNextPage = true;
+  bool pastEventsHasNextPage = true;
 
   @override
   Widget build(BuildContext context) {
     final t = Translations.of(context);
     final colorScheme = Theme.of(context).colorScheme;
+    final userId = AuthUtils.getUserId(context);
     return Scaffold(
       backgroundColor: colorScheme.background,
       appBar: LemonAppBar(
@@ -113,28 +119,124 @@ class _MyEventTicketsListViewState extends State<MyEventTicketsListView> {
             ),
           ),
           if (selectedReservationType == ReservationType.upcoming)
-            BlocBuilder<UpcomingEventsListingBloc, BaseEventsListingState>(
-              builder: (context, state) => state.when(
-                loading: () => Loading.defaultLoading(context),
-                failure: () => EmptyList(
-                  emptyText: t.common.somethingWrong,
-                ),
-                fetched: (eventsList, _) => EventReservationsListView(
-                  eventsList: eventsList,
+            Query$GetUpcomingEvents$Widget(
+              options: Options$Query$GetUpcomingEvents(
+                variables: Variables$Query$GetUpcomingEvents(
+                  id: userId,
+                  limit: 20,
                 ),
               ),
+              builder: (
+                result, {
+                fetchMore,
+                refetch,
+              }) {
+                final eventsList = (result.parsedData?.getUpcomingEvents ?? [])
+                    .map(
+                      (item) => Event.fromJson(
+                        item.toJson(),
+                      ),
+                    )
+                    .toList();
+                if (result.isLoading && eventsList.isEmpty) {
+                  return Loading.defaultLoading(context);
+                }
+                if (result.hasException) {
+                  return EmptyList(
+                    emptyText: t.common.somethingWrong,
+                  );
+                }
+                return EventReservationsListView(
+                  eventsList: eventsList,
+                  onFetchMore: () {
+                    fetchMore?.call(
+                      FetchMoreOptions$Query$GetUpcomingEvents(
+                        variables: Variables$Query$GetUpcomingEvents(
+                          id: userId,
+                          limit: 20,
+                          skip: eventsList.length,
+                        ),
+                        updateQuery: (prev, next) {
+                          final prevEvents = prev?['getUpcomingEvents'] ?? [];
+                          final nextEvents =
+                              (next?['getUpcomingEvents'] as List<dynamic>?) ??
+                                  [];
+                          next?['getUpcomingEvents'] = [
+                            ...prevEvents,
+                            ...nextEvents,
+                          ];
+                          if (nextEvents.isEmpty) {
+                            setState(() {
+                              upcomingEventsHasNextPage = false;
+                            });
+                          }
+                          return next;
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
             ),
           if (selectedReservationType == ReservationType.past)
-            BlocBuilder<PastEventsListingBloc, BaseEventsListingState>(
-              builder: (context, state) => state.when(
-                loading: () => Loading.defaultLoading(context),
-                failure: () => EmptyList(
-                  emptyText: t.common.somethingWrong,
-                ),
-                fetched: (eventsList, _) => EventReservationsListView(
-                  eventsList: eventsList,
+            Query$GetPastEvents$Widget(
+              options: Options$Query$GetPastEvents(
+                variables: Variables$Query$GetPastEvents(
+                  id: userId,
+                  limit: 20,
+                  skip: 0,
                 ),
               ),
+              builder: (
+                result, {
+                fetchMore,
+                refetch,
+              }) {
+                final eventsList = (result.parsedData?.getPastEvents ?? [])
+                    .map(
+                      (item) => Event.fromJson(
+                        item.toJson(),
+                      ),
+                    )
+                    .toList();
+                if (result.isLoading && eventsList.isEmpty) {
+                  return Loading.defaultLoading(context);
+                }
+                if (result.hasException) {
+                  return EmptyList(
+                    emptyText: t.common.somethingWrong,
+                  );
+                }
+                return EventReservationsListView(
+                  eventsList: eventsList,
+                  onFetchMore: () {
+                    fetchMore?.call(
+                      FetchMoreOptions$Query$GetPastEvents(
+                        variables: Variables$Query$GetPastEvents(
+                          id: userId,
+                          limit: 20,
+                          skip: eventsList.length,
+                        ),
+                        updateQuery: (prev, next) {
+                          final prevEvents = prev?['getPastEvents'] ?? [];
+                          final nextEvents =
+                              (next?['getPastEvents'] as List<dynamic>?) ?? [];
+                          next?['getPastEvents'] = [
+                            ...prevEvents,
+                            ...nextEvents,
+                          ];
+                          if (nextEvents.isEmpty) {
+                            setState(() {
+                              pastEventsHasNextPage = false;
+                            });
+                          }
+                          return next;
+                        },
+                      ),
+                    );
+                  },
+                );
+              },
             ),
         ],
       ),
