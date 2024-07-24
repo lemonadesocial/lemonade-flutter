6dd1b2ba58d4256740d3bc58da9e75548e11c7fc
sieuhuflit <sieuhuflit@gmail.com>
2024-07-24 09:27:12 +0700

    feat: add filter team member role bar

--- lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_team_members_setting_page/event_team_members_setting_page.dart
+++ lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_team_members_setting_page/event_team_members_setting_page.dart
@@ -1,18 +1,15 @@
 import 'package:app/core/application/event/get_event_detail_bloc/get_event_detail_bloc.dart';
 import 'package:app/core/domain/event/entities/event.dart';
+import 'package:app/core/presentation/pages/event/event_control_panel_page/sub_pages/event_team_members_setting_page/widgets/event_team_members_search_bar.dart';
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
-import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/core/presentation/widgets/common/button/lemon_outline_button_widget.dart';
 import 'package:app/core/utils/string_utils.dart';
-import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
-import 'package:app/theme/color.dart';
 import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
-import 'package:app/theme/typo.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 @RoutePage()
 class EventTeamMembersSettingPage extends StatelessWidget {
@@ -50,10 +47,6 @@ class _EventTeamMembersSettingPageViewState
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
     final t = Translations.of(context);
-    final border = OutlineInputBorder(
-      borderRadius: BorderRadius.circular(LemonRadius.small),
-      borderSide: const BorderSide(color: Colors.transparent),
-    );
     return Scaffold(
       appBar: LemonAppBar(
         backgroundColor: colorScheme.background,
@@ -65,90 +58,44 @@ class _EventTeamMembersSettingPageViewState
         mainAxisSize: MainAxisSize.min,
         children: [
           Padding(
-            padding: EdgeInsets.only(
-              left: Spacing.xSmall,
-              right: Spacing.xSmall,
-              top: Spacing.xSmall,
-              bottom: Spacing.superExtraSmall,
+            padding: EdgeInsets.symmetric(
+              horizontal: Spacing.xSmall,
             ),
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: SizedBox(
-                    height: 42.w,
-                    child: TextField(
-                      controller: _textController,
-                      onChanged: (v) {},
-                      cursorColor: colorScheme.onSecondary,
-                      decoration: InputDecoration(
-                        fillColor: LemonColor.atomicBlack,
-                        hintStyle: Typo.medium.copyWith(
-                          color: colorScheme.onSecondary,
-                        ),
-                        contentPadding: EdgeInsets.zero,
-                        hintText: StringUtils.capitalize(t.common.search),
-                        filled: true,
-                        isDense: true,
-                        enabledBorder: border,
-                        focusedBorder: border,
-                        prefixIcon: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            ThemeSvgIcon(
-                              color: colorScheme.onSecondary,
-                              builder: (filter) => Assets.icons.icSearch.svg(
-                                colorFilter: filter,
-                                width: Sizing.mSmall,
-                                height: Sizing.mSmall,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  width: Spacing.xSmall,
-                ),
-                _AddTeamMemberButton(),
-              ],
+            child: EventTeamMembersSearchBar(
+              textController: _textController,
             ),
           ),
-        ],
-      ),
-    );
-  }
-}
-
-class _AddTeamMemberButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: Sizing.medium,
-      height: Sizing.medium,
-      padding: EdgeInsets.all(LemonRadius.xSmall),
-      decoration: ShapeDecoration(
-        color: LemonColor.acidGreen,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(LemonRadius.normal),
-        ),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            width: Sizing.xSmall,
-            height: Sizing.xSmall,
-            clipBehavior: Clip.antiAlias,
-            decoration: const BoxDecoration(),
-            child: ThemeSvgIcon(
-              color: LemonColor.paleViolet,
-              builder: (filter) => Assets.icons.icAdd.svg(
-                colorFilter: filter,
+          SizedBox(
+            height: Spacing.xSmall,
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+              left: Spacing.small,
+            ),
+            child: SizedBox(
+              height: Sizing.medium,
+              child: ListView.separated(
+                scrollDirection: Axis.horizontal,
+                separatorBuilder: (context, index) =>
+                    SizedBox(width: Spacing.extraSmall),
+                itemCount: 5,
+                itemBuilder: (context, index) {
+                  final selected = index == 0;
+                  return LemonOutlineButton(
+                    onTap: () {},
+                    textColor: selected == true
+                        ? colorScheme.onPrimary
+                        : colorScheme.onSecondary,
+                    backgroundColor: selected == true
+                        ? colorScheme.outline
+                        : Colors.transparent,
+                    borderColor: selected == true
+                        ? Colors.transparent
+                        : colorScheme.outline,
+                    label: StringUtils.capitalize("Co-host"),
+                    radius: BorderRadius.circular(LemonRadius.button),
+                  );
+                },
               ),
             ),
           ),
