5f7a81df2849850ff9f232cb80157c327cbd3545
Thinh CP <thinh@coderpush.com>
2024-08-13 14:42:38 +0700

    chore: handle pagination for notifications list

--- lib/core/presentation/pages/notification/widgets/notifications_list_view.dart
+++ lib/core/presentation/pages/notification/widgets/notifications_list_view.dart
@@ -12,6 +12,7 @@ import 'package:app/graphql/backend/notification/query/get_notifications.graphql
 import 'package:app/graphql/backend/schema.graphql.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/injection/register_module.dart';
+import 'package:app/theme/spacing.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
@@ -28,17 +29,14 @@ class NotificationListView extends StatefulWidget {
 }
 
 class _NotificationListViewState extends State<NotificationListView> {
-  final GlobalKey<AnimatedListState> _notificationList =
-      GlobalKey<AnimatedListState>();
+  List<String> removedNotificationIds = [];
 
   void removeItem(
     context, {
     required int index,
     required notification_entities.Notification notification,
-    bool isDismiss = false,
-    Function? refetch,
   }) async {
-    final result = await showFutureLoadingDialog(
+    final response = await showFutureLoadingDialog(
       context: context,
       future: () => getIt<NotificationRepository>().deleteNotifications(
         input: DeleteNotificationsInput(
@@ -48,31 +46,14 @@ class _NotificationListViewState extends State<NotificationListView> {
         ),
       ),
     );
-    result.result?.fold((l) => null, (success) {
+    response.result?.fold((l) => null, (success) {
       if (!success) return;
-      _notificationList.currentState?.removeItem(
-        index,
-        (context, animation) {
-          return isDismiss
-              ? Container()
-              : SizeTransition(
-                  sizeFactor: animation,
-                  child: AnimatedBuilder(
-                    animation: animation,
-                    builder: (context, child) => AnimatedOpacity(
-                      opacity: animation.value,
-                      duration: const Duration(milliseconds: 300),
-                      child: child,
-                    ),
-                    child: NotificationCard(
-                      index: index,
-                      notification: notification,
-                    ),
-                  ),
-                );
-        },
-      );
-      refetch?.call();
+      setState(() {
+        removedNotificationIds = [
+          ...removedNotificationIds,
+          notification.id ?? '',
+        ];
+      });
     });
   }
 
@@ -85,7 +66,7 @@ class _NotificationListViewState extends State<NotificationListView> {
           options: Options$Query$GetNotifications(
             fetchPolicy: FetchPolicy.networkOnly,
             variables: Variables$Query$GetNotifications(
-              limit: 100,
+              limit: 20,
               skip: 0,
               type: widget.filter,
             ),
@@ -95,7 +76,8 @@ class _NotificationListViewState extends State<NotificationListView> {
             refetch,
             fetchMore,
           }) {
-            if (result.isLoading || result.source == QueryResultSource.cache) {
+            if ((result.isLoading) &&
+                (result.parsedData?.getNotifications ?? []).isEmpty == true) {
               return Center(
                 child: Loading.defaultLoading(context),
               );
@@ -131,24 +113,25 @@ class _NotificationListViewState extends State<NotificationListView> {
                 if (notification is ScrollEndNotification) {
                   if (notification.metrics.pixels ==
                       notification.metrics.maxScrollExtent) {
-                    // TODO:
-                    // fetchMore?.call(
-                    //   FetchMoreOptions$Query$GetNotifications(
-                    //     variables: Variables$Query$GetNotifications(
-                    //       limit: 10,
-                    //       skip: notifications.length,
-                    //     ),
-                    //     updateQuery: (prevResult, fetchMoreResult) {
-                    //       final List<dynamic> finalList = [
-                    //         ...(prevResult?['getNotifications'] ?? []) as List<dynamic>,
-                    //         ...(fetchMoreResult?['getNotifications'] ?? []) as List<dynamic>,
-                    //       ];
-                    //       fetchMoreResult?['getNotifications'] = finalList;
-
-                    //       return fetchMoreResult;
-                    //     },
-                    //   ),
-                    // );
+                    fetchMore?.call(
+                      FetchMoreOptions$Query$GetNotifications(
+                        variables: Variables$Query$GetNotifications(
+                          limit: 20,
+                          skip: notifications.length,
+                          type: widget.filter,
+                        ),
+                        updateQuery: (prevResult, fetchMoreResult) {
+                          final List<dynamic> finalList = [
+                            ...(prevResult?['getNotifications'] ?? [])
+                                as List<dynamic>,
+                            ...(fetchMoreResult?['getNotifications'] ?? [])
+                                as List<dynamic>,
+                          ];
+                          fetchMoreResult?['getNotifications'] = finalList;
+                          return fetchMoreResult;
+                        },
+                      ),
+                    );
                   }
                 }
                 return true;
@@ -157,30 +140,40 @@ class _NotificationListViewState extends State<NotificationListView> {
                 slivers: [
                   SliverPadding(
                     padding: EdgeInsets.only(bottom: 150.h),
-                    sliver: SliverAnimatedList(
-                      // Add more space at bottom of screen
-                      key: _notificationList,
-                      itemBuilder: (ctx, index, animation) => NotificationCard(
-                        key: ValueKey(notifications[index].id ?? ''),
-                        index: index,
-                        notification: notifications[index],
-                        onTap: () {
-                          NavigationUtils.handleNotificationNavigate(
-                            context,
-                            notifications[index],
+                    sliver: SliverList.builder(
+                      itemBuilder: (ctx, index) {
+                        if (index == notifications.length) {
+                          return Padding(
+                            padding: EdgeInsets.symmetric(
+                              vertical: Spacing.smMedium,
+                            ),
+                            child: Loading.defaultLoading(context),
                           );
-                        },
-                        onRemove: (index, notification, isDismiss) {
-                          removeItem(
-                            context,
-                            index: index,
-                            notification: notification,
-                            isDismiss: isDismiss ?? false,
-                            refetch: refetch,
-                          );
-                        },
-                      ),
-                      initialItemCount: notifications.length,
+                        }
+                        if (removedNotificationIds
+                            .contains(notifications[index].id)) {
+                          return const SizedBox.shrink();
+                        }
+                        return NotificationCard(
+                          key: ValueKey(notifications[index].id ?? ''),
+                          index: index,
+                          notification: notifications[index],
+                          onTap: () {
+                            NavigationUtils.handleNotificationNavigate(
+                              context,
+                              notifications[index],
+                            );
+                          },
+                          onRemove: (index, notification, isDismiss) {
+                            removeItem(
+                              context,
+                              index: index,
+                              notification: notification,
+                            );
+                          },
+                        );
+                      },
+                      itemCount: notifications.length + 1,
                     ),
                   ),
                 ],
