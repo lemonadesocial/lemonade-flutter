d2325e8124be52a4d4430633708ae5dac842b1c3
sieuhuflit <sieuhuflit@gmail.com>
2024-10-28 10:07:19 +0700

    feat: remake timezone and support search timezone

--- lib/core/presentation/pages/event/create_event/widgets/timezone_select_bottomsheet.dart
+++ lib/core/presentation/pages/event/create_event/widgets/timezone_select_bottomsheet.dart
@@ -2,22 +2,22 @@ import 'package:app/core/application/event/event_datetime_settings_bloc/event_da
 import 'package:app/core/application/event/get_event_detail_bloc/get_event_detail_bloc.dart';
 import 'package:app/core/constants/event/event_constants.dart';
 import 'package:app/core/domain/event/entities/event.dart';
+import 'package:app/core/presentation/widgets/bottomsheet_grabber/bottomsheet_grabber.dart';
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
-import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
+import 'package:app/core/presentation/widgets/lemon_text_field.dart';
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 import 'package:app/core/utils/snackbar_utils.dart';
 import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/theme/color.dart';
-import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
+import 'package:app/theme/typo.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:formz/formz.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
-import 'package:wheel_picker/wheel_picker.dart';
-
-const double timezoneWheelSize = 225;
+import 'package:slang/builder/utils/string_extensions.dart';
 
 class TimezoneSelectBottomSheet extends StatefulWidget {
   const TimezoneSelectBottomSheet({super.key, this.event});
@@ -29,36 +29,35 @@ class TimezoneSelectBottomSheet extends StatefulWidget {
 }
 
 class _TimezoneSelectBottomSheetState extends State<TimezoneSelectBottomSheet> {
-  late String? selectedTimezone = '';
+  final TextEditingController _searchController = TextEditingController();
+  List<Map<String, String>> _filteredTimezones = EventConstants.timezoneOptions;
+
   @override
   void initState() {
     super.initState();
-    final timezone = context.read<EventDateTimeSettingsBloc>().state.timezone;
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  void _onSearchChanged(String value) {
     setState(() {
-      selectedTimezone = timezone;
+      _filteredTimezones = EventConstants.timezoneOptions
+          .where(
+            (timezone) =>
+                timezone['text']!.toLowerCase().contains(value.toLowerCase()),
+          )
+          .toList();
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    final t = Translations.of(context);
     final colorScheme = Theme.of(context).colorScheme;
-    final timezone = context.read<EventDateTimeSettingsBloc>().state.timezone;
-    final index = EventConstants.timezoneOptions
-        .indexWhere((element) => element['value'] == timezone);
-    late final timezoneWheelController = WheelPickerController(
-      itemCount: EventConstants.timezoneOptions.length,
-      initialIndex: index,
-    );
-    const textStyle = TextStyle(fontSize: 10.0, height: 3, color: Colors.white);
-    final wheelStyle = WheelPickerStyle(
-      size: timezoneWheelSize,
-      itemExtent: textStyle.fontSize! * textStyle.height!,
-      squeeze: 1.25,
-      diameterRatio: 1,
-      surroundingOpacity: .25,
-      magnification: 1.2,
-    );
+    final t = Translations.of(context);
     return BlocListener<EventDateTimeSettingsBloc, EventDateTimeSettingsState>(
       listener: (context, state) async {
         if (state.isValid == true &&
@@ -80,103 +79,121 @@ class _TimezoneSelectBottomSheetState extends State<TimezoneSelectBottomSheet> {
           AutoRouter.of(context).pop();
         }
       },
-      child: SingleChildScrollView(
-        controller: ModalScrollController.of(context),
-        padding: EdgeInsets.only(
-          bottom: MediaQuery.of(context).viewInsets.bottom,
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: LemonColor.atomicBlack,
-          ),
-          child: SafeArea(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                LemonAppBar(
-                  backgroundColor: LemonColor.atomicBlack,
-                  title: t.event.timezoneSetting.chooseTimezone,
+      child: Container(
+        padding:
+            EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+        color: colorScheme.secondaryContainer,
+        child: SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const BottomSheetGrabber(),
+              LemonAppBar(
+                backgroundColor: Colors.transparent,
+                title: t.event.timezoneSetting.chooseTimezone,
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: Spacing.smMedium,
                 ),
-                Stack(
-                  children: [
-                    _centerBar(context),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width,
-                      child: BlocBuilder<EventDateTimeSettingsBloc,
-                          EventDateTimeSettingsState>(
-                        builder: (context, state) {
-                          return WheelPicker(
-                            style: wheelStyle,
-                            controller: timezoneWheelController,
-                            builder: (BuildContext context, int index) {
-                              String? textDisplay =
-                                  EventConstants.timezoneOptions.toList()[index]
-                                          ['text'] ??
-                                      '';
-                              return Text(
-                                textDisplay,
-                                style: textStyle,
-                              );
-                            },
-                            selectedIndexColor: colorScheme.onPrimary,
-                            onIndexChanged: (index) {
-                              final element = EventConstants.timezoneOptions
-                                  .elementAt(index);
-                              setState(() {
-                                selectedTimezone = element['value'];
-                              });
-                            },
-                          );
-                        },
+                child: LemonTextField(
+                  radius: 16.r,
+                  controller: _searchController,
+                  onChange: _onSearchChanged,
+                  hintText: t.common.search.capitalize(),
+                  leadingIcon: Padding(
+                    padding: EdgeInsets.only(
+                        left: Spacing.small, right: Spacing.xSmall),
+                    child: ThemeSvgIcon(
+                      color: colorScheme.onSecondary,
+                      builder: (filter) => Assets.icons.icSearch.svg(
+                        colorFilter: filter,
+                        width: 20.w,
+                        height: 20.w,
                       ),
                     ),
-                  ],
+                  ),
+                  placeholderStyle: Typo.medium.copyWith(
+                    color: colorScheme.onSecondary,
+                  ),
                 ),
-                BlocBuilder<EventDateTimeSettingsBloc,
-                    EventDateTimeSettingsState>(
-                  builder: (context, state) {
-                    return Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: Spacing.smMedium,
-                        horizontal: Spacing.xSmall,
-                      ),
-                      child: LinearGradientButton.primaryButton(
-                        onTap: () {
-                          context.read<EventDateTimeSettingsBloc>().add(
-                                EventDateTimeSettingsEventSaveChangesTimezone(
-                                  event: widget.event,
-                                  timezone: selectedTimezone ?? '',
-                                ),
-                              );
-                        },
-                        label: t.common.actions.saveChanges,
-                        loadingWhen:
-                            state.status == FormzSubmissionStatus.inProgress,
-                      ),
+              ),
+              SizedBox(height: Spacing.smMedium),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: _filteredTimezones.length,
+                  itemBuilder: (context, index) {
+                    return TimezoneItem(
+                      event: widget.event,
+                      text: _filteredTimezones[index]['text'] ?? '',
+                      value: _filteredTimezones[index]['value'] ?? '',
                     );
                   },
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+}
 
-  Widget _centerBar(BuildContext context) {
+class TimezoneItem extends StatelessWidget {
+  final Event? event;
+  final String value;
+  final String text;
+
+  const TimezoneItem({
+    super.key,
+    required this.event,
+    required this.value,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
-    return Container(
-      height: timezoneWheelSize,
-      padding: EdgeInsets.symmetric(horizontal: Spacing.smMedium),
-      child: Center(
-        child: Container(
-          height: Sizing.medium,
-          decoration: BoxDecoration(
-            color: colorScheme.secondary,
-            borderRadius: BorderRadius.circular(8.0),
-          ),
+    return InkWell(
+      onTap: () {
+        context.read<EventDateTimeSettingsBloc>().add(
+              EventDateTimeSettingsEventSaveChangesTimezone(
+                event: event,
+                timezone: value,
+              ),
+            );
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: Spacing.smMedium,
+          vertical: Spacing.small,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Text(
+                text,
+                overflow: TextOverflow.visible,
+                softWrap: true,
+                style: Typo.medium.copyWith(
+                  color: colorScheme.onPrimary,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: Spacing.small),
+              child: ThemeSvgIcon(
+                color: colorScheme.onSecondary,
+                builder: (filter) => Assets.icons.icArrowRight.svg(
+                  colorFilter: filter,
+                  width: 15.w,
+                  height: 15.w,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
