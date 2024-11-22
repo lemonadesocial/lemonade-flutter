2fb47d733ec533ce6785201a40c566326ba5465f
sieuhuflit <sieuhuflit@gmail.com>
2024-11-22 10:39:46 +0700

    feat: implement select instruction dropdown

--- lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_add_instructions_setting_page/event_add_instructions_setting_page.dart
+++ lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_add_instructions_setting_page/event_add_instructions_setting_page.dart
@@ -4,8 +4,12 @@ import 'package:app/core/presentation/widgets/common/button/linear_gradient_butt
 import 'package:app/core/presentation/widgets/lemon_text_field.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/theme/color.dart';
+import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:app/gen/assets.gen.dart';
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
 
 class EventAddInstructionsSettingPage extends StatefulWidget {
   final String? instruction;
@@ -51,6 +55,8 @@ class _EventAddInstructionsSettingPageState
   Widget build(BuildContext context) {
     final t = Translations.of(context);
     final colorScheme = Theme.of(context).colorScheme;
+    final bool isEditing = widget.instruction?.isNotEmpty ?? false;
+
     return Container(
       padding: EdgeInsets.only(
         bottom: MediaQuery.of(context).viewInsets.bottom != 0
@@ -65,8 +71,34 @@ class _EventAddInstructionsSettingPageState
           const BottomSheetGrabber(),
           LemonAppBar(
             backgroundColor: LemonColor.chineseBlack,
-            title: t.event.instructions.addInstructions,
+            title: isEditing
+                ? t.event.instructions.editInstructions
+                : t.event.instructions.addInstructions,
             onPressBack: () => Navigator.of(context, rootNavigator: true).pop(),
+            actions: isEditing
+                ? [
+                    Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: Spacing.smMedium),
+                      child: InkWell(
+                        onTap: () {
+                          editingController.clear();
+                          _onChange();
+                          widget.onConfirm?.call("");
+                          AutoRouter.of(context).pop();
+                        },
+                        child: ThemeSvgIcon(
+                          color: LemonColor.coralReef,
+                          builder: (filter) => Assets.icons.icDelete.svg(
+                            colorFilter: filter,
+                            width: Sizing.small,
+                            height: Sizing.small,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]
+                : [],
           ),
           Padding(
             padding: EdgeInsets.symmetric(horizontal: Spacing.xSmall),
