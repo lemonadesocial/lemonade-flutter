d420250a84672a9b34ad1c6d5ef140bcc140b9fe
Sieu Thai <sieuhuflit@gmail.com>
2025-01-07 16:33:56 +0700

    fix: logout issue (#838)

--- lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_ticket_tier_setting_page/sub_pages/event_create_ticket_tier_page/widgets/get_event_currencies_builder.dart
+++ lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_ticket_tier_setting_page/sub_pages/event_create_ticket_tier_page/widgets/get_event_currencies_builder.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/application/auth/auth_bloc.dart';
 import 'package:app/core/domain/event/entities/event_currency.dart';
 import 'package:app/core/domain/event/input/get_event_currencies_input/get_event_currencies_input.dart';
 import 'package:app/core/domain/event/repository/event_ticket_repository.dart';
@@ -5,6 +6,7 @@ import 'package:app/core/failure.dart';
 import 'package:app/injection/register_module.dart';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GetEventCurrenciesBuilder extends StatelessWidget {
@@ -22,11 +24,18 @@ class GetEventCurrenciesBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final isLoggedIn = context.watch<AuthBloc>().state.maybeWhen(
+          orElse: () => false,
+          authenticated: (_) => true,
+        );
     return FutureBuilder<Either<Failure, List<EventCurrency>>>(
-      future: getIt<EventTicketRepository>().getEventCurrencies(
-        input: GetEventCurrenciesInput(id: eventId, used: false),
-        fetchPolicy: FetchPolicy.networkOnly,
-      ),
+      future: isLoggedIn
+          ? getIt<EventTicketRepository>().getEventCurrencies(
+              input: GetEventCurrenciesInput(id: eventId, used: false),
+              fetchPolicy: FetchPolicy.networkOnly,
+            )
+          // ignore: null_argument_to_non_null_type
+          : Future.value(null),
       builder: (context, snapshot) {
         final currencies = snapshot.data?.getOrElse(() => []) ?? [];
         final isLoading = snapshot.connectionState == ConnectionState.waiting;
