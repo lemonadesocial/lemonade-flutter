bfcf814fa0c8fcd3da65fa5079a5ff973f518b4d
sieuhuflit <sieuhuflit@gmail.com>
2025-02-12 09:47:40 +0700

    chore: clean up

--- lib/core/service/crash_analytics/crash_analytics_service.dart
+++ lib/core/service/crash_analytics/crash_analytics_service.dart
@@ -6,6 +6,9 @@ abstract class CrashAnalyticsService {
   void setTag(String key, String value);
   void captureError(dynamic error, StackTrace? stacktrace);
   void captureMessage(String message, List<dynamic>? params);
-  void addBreadcrumb(
-      {String? category, String? message, Map<String, dynamic>? params,});
+  void addBreadcrumb({
+    String? category,
+    String? message,
+    Map<String, dynamic>? params,
+  });
 }
