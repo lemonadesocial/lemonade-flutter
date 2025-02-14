46c631b81f087c2ad8a326c538b0f1221428b14e
sieuhuflit <sieuhuflit@gmail.com>
2025-02-14 09:16:22 +0700

    feat: hide reward in setting

--- lib/core/presentation/pages/event/create_event/sub_pages/widgets/create_event_registration_section.dart
+++ lib/core/presentation/pages/event/create_event/sub_pages/widgets/create_event_registration_section.dart
@@ -121,7 +121,6 @@ class CreateEventRegistrationSection extends StatelessWidget {
         isEditMode ? initialEvent?.paymentTicketDiscounts?.length ?? 0 : 0;
     final applicationFormCount =
         isEditMode ? initialEvent?.applicationQuestions?.length ?? 0 : 0;
-    final rewardCount = isEditMode ? initialEvent?.rewards?.length ?? 0 : 0;
 
     return Container(
       width: double.infinity,
@@ -381,30 +380,6 @@ class CreateEventRegistrationSection extends StatelessWidget {
                     value: applicationFormCount > 0
                         ? applicationFormCount.toString()
                         : null,
-                  ),
-                ],
-              ),
-            ),
-            SizedBox(height: Spacing.xSmall),
-            Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: LemonColor.chineseBlack,
-                borderRadius: BorderRadius.circular(LemonRadius.medium),
-              ),
-              child: Column(
-                children: [
-                  _buildSettingRow(
-                    context,
-                    icon: Assets.icons.icGift,
-                    title: t.event.configuration.rewards,
-                    trailingIcon: Assets.icons.icArrowRight,
-                    onTap: () {
-                      AutoRouter.of(context).navigate(
-                        const EventRewardSettingRoute(),
-                      );
-                    },
-                    value: rewardCount > 0 ? rewardCount.toString() : null,
                   ),
                 ],
               ),
