ad27e790b961078494c966b7361a968b75974430
sieuhuflit <sieuhuflit@gmail.com>
2025-03-05 14:39:33 +0700

    chore: clean up

--- lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_approval_setting_page/widgets/join_request_item/event_confirmed_join_request_item.dart
+++ lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_approval_setting_page/widgets/join_request_item/event_confirmed_join_request_item.dart
@@ -1,9 +1,11 @@
+import 'package:app/core/application/event/get_event_detail_bloc/get_event_detail_bloc.dart';
 import 'package:app/core/domain/event/entities/event_join_request.dart';
 import 'package:app/core/presentation/pages/event/event_control_panel_page/sub_pages/event_approval_setting_page/widgets/join_request_user_avatar.dart';
 import 'package:app/router/app_router.gr.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EventConfirmedJoinRequestItem extends StatelessWidget {
   final EventJoinRequest eventJoinRequest;
@@ -18,13 +20,19 @@ class EventConfirmedJoinRequestItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
+    final event = context.read<GetEventDetailBloc>().state.maybeWhen(
+          orElse: () => null,
+          fetched: (event) => event,
+        );
     return InkWell(
       onTap: () {
-        // AutoRouter.of(context).push(
-        //   EventJoinRequestDetailRoute(
-        //     eventJoinRequest: eventJoinRequest,
-        //   ),
-        // );
+        AutoRouter.of(context).push(
+          EventGuestDetailRoute(
+            eventId: event?.id ?? '',
+            userId: eventJoinRequest.user ?? '',
+            email: eventJoinRequest.nonLoginUser?.email ?? '',
+          ),
+        );
       },
       child: Column(
         mainAxisSize: MainAxisSize.min,
