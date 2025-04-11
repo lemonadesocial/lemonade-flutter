035d619fa320a9df8d090da0021ee3fd738808d6
Thinh CP <thinh@coderpush.com>
2025-04-11 15:12:09 +0700

    fix: cover photo setting ui

--- lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_photos_setting_page/widgets/cover_photo_item.dart
+++ lib/core/presentation/pages/event/event_control_panel_page/sub_pages/event_photos_setting_page/widgets/cover_photo_item.dart
@@ -8,7 +8,6 @@ import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CoverPhotoItem extends StatelessWidget {
   final DbFile? photo;
@@ -24,80 +23,83 @@ class CoverPhotoItem extends StatelessWidget {
     final t = Translations.of(context);
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Container(
-      height: 200.w,
-      decoration: BoxDecoration(
-        border: Border.all(color: colorScheme.outline),
-        borderRadius: BorderRadius.circular(LemonRadius.medium),
-      ),
-      child: InkWell(
-        onTap: onEdit,
-        child: Builder(
-          builder: (context) {
-            if (photo != null) {
-              return Stack(
-                children: [
-                  Positioned.fill(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(LemonRadius.medium),
-                      child: CachedNetworkImage(
-                        fit: BoxFit.cover,
-                        imageUrl: ImageUtils.generateUrl(file: photo),
-                        placeholder: (_, __) =>
-                            ImagePlaceholder.defaultPlaceholder(),
-                        errorWidget: (_, __, ___) =>
-                            ImagePlaceholder.defaultPlaceholder(),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: Spacing.xSmall,
-                    right: Spacing.xSmall,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(
-                        vertical: Spacing.superExtraSmall,
-                        horizontal: Spacing.xSmall,
-                      ),
-                      decoration: BoxDecoration(
-                        color: colorScheme.background.withOpacity(0.7),
-                        borderRadius:
-                            BorderRadius.circular(LemonRadius.extraSmall),
-                        border: Border.all(color: colorScheme.outline),
-                      ),
-                      child: Text(
-                        t.event.eventPhotos.editCover,
-                        style: Typo.small.copyWith(
-                          color: colorScheme.onPrimary,
-                          fontWeight: FontWeight.w600,
+    return AspectRatio(
+      aspectRatio: 1,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border.all(color: colorScheme.outline),
+          borderRadius: BorderRadius.circular(LemonRadius.medium),
+        ),
+        child: InkWell(
+          onTap: onEdit,
+          child: Builder(
+            builder: (context) {
+              if (photo != null) {
+                return Stack(
+                  children: [
+                    Positioned.fill(
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(LemonRadius.medium),
+                        child: CachedNetworkImage(
+                          fit: BoxFit.cover,
+                          imageUrl: ImageUtils.generateUrl(file: photo),
+                          placeholder: (_, __) =>
+                              ImagePlaceholder.defaultPlaceholder(),
+                          errorWidget: (_, __, ___) =>
+                              ImagePlaceholder.defaultPlaceholder(),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              );
-            }
+                    Positioned(
+                      top: Spacing.xSmall,
+                      right: Spacing.xSmall,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                          vertical: Spacing.superExtraSmall,
+                          horizontal: Spacing.xSmall,
+                        ),
+                        decoration: BoxDecoration(
+                          color: colorScheme.background.withOpacity(0.7),
+                          borderRadius:
+                              BorderRadius.circular(LemonRadius.extraSmall),
+                          border: Border.all(color: colorScheme.outline),
+                        ),
+                        child: Text(
+                          t.event.eventPhotos.editCover,
+                          style: Typo.small.copyWith(
+                            color: colorScheme.onPrimary,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              }
 
-            return Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ThemeSvgIcon(
-                    color: colorScheme.onSecondary,
-                    builder: (colorFilter) => Assets.icons.icAddPhoto.svg(
-                      colorFilter: colorFilter,
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ThemeSvgIcon(
+                      color: colorScheme.onSecondary,
+                      builder: (colorFilter) => Assets.icons.icAddPhoto.svg(
+                        colorFilter: colorFilter,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: Spacing.xSmall),
-                  Text(
-                    t.event.eventPhotos.uploadHint,
-                    style: Typo.small.copyWith(
-                      color: colorScheme.onSurfaceVariant,
+                    SizedBox(height: Spacing.xSmall),
+                    Text(
+                      t.event.eventPhotos.uploadHint,
+                      style: Typo.small.copyWith(
+                        color: colorScheme.onSurfaceVariant,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            );
-          },
+                  ],
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
