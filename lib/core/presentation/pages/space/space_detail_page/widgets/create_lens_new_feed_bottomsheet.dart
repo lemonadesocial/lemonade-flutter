06fb67677bfe403214868c4b9d0ca149f03e8984
sieuhuflit <sieuhuflit@gmail.com>
2025-04-25 13:17:43 +0700

    feat: support add admins

--- lib/core/presentation/pages/space/space_detail_page/widgets/create_lens_new_feed_bottomsheet.dart
+++ lib/core/presentation/pages/space/space_detail_page/widgets/create_lens_new_feed_bottomsheet.dart
@@ -3,8 +3,11 @@ import 'package:app/core/presentation/widgets/bottomsheet_grabber/bottomsheet_gr
 import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
 import 'package:app/core/presentation/widgets/common/button/linear_gradient_button_widget.dart';
 import 'package:app/core/presentation/widgets/lemon_text_field.dart';
+import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
+import 'package:app/gen/assets.gen.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/theme/color.dart';
+import 'package:app/theme/sizing.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
@@ -26,7 +29,7 @@ class CreateLensNewFeedBottomSheetState
     extends State<CreateLensNewFeedBottomSheet> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
-  final List<String> _selectedAdmins = [];
+  final List<String> _admins = ['']; // Initialize with one empty field
 
   @override
   void initState() {
@@ -42,10 +45,29 @@ class CreateLensNewFeedBottomSheetState
     super.dispose();
   }
 
+  void _addNewAdmin() {
+    setState(() {
+      _admins.add('');
+    });
+  }
+
+  void _removeAdmin(int index) {
+    if (_admins.length > 1) {
+      setState(() {
+        _admins.removeAt(index);
+      });
+    }
+  }
+
+  void _updateAdmin(int index, String value) {
+    setState(() {
+      _admins[index] = value;
+    });
+  }
+
   bool get _isFormValid =>
       _nameController.text.isNotEmpty &&
-      _descriptionController.text.isNotEmpty &&
-      _selectedAdmins.isNotEmpty;
+      _admins.any((admin) => admin.isNotEmpty);
 
   @override
   Widget build(BuildContext context) {
@@ -104,7 +126,7 @@ class CreateLensNewFeedBottomSheetState
                   ),
                   SizedBox(height: Spacing.medium),
 
-                  // Admins field
+                  // Admins section
                   Text(
                     t.space.lens.admins,
                     style: TextStyle(
@@ -113,9 +135,28 @@ class CreateLensNewFeedBottomSheetState
                     ),
                   ),
                   SizedBox(height: Spacing.xSmall),
-                  // TODO: Implement admin selection widget
-                  // This could be a separate widget that shows selected admins
-                  // and allows adding more through a search interface
+                  Column(
+                    children: [
+                      ..._admins.asMap().entries.map(
+                            (entry) => Column(
+                              children: [
+                                _AdminField(
+                                  value: entry.value,
+                                  onChanged: (value) =>
+                                      _updateAdmin(entry.key, value),
+                                  onRemove: () => _removeAdmin(entry.key),
+                                  removable: _admins.length > 1,
+                                ),
+                                SizedBox(height: Spacing.xSmall),
+                              ],
+                            ),
+                          ),
+                      SizedBox(height: Spacing.superExtraSmall),
+                      _AddButton(
+                        onPress: _addNewAdmin,
+                      ),
+                    ],
+                  ),
                 ],
               ),
             ),
@@ -134,9 +175,10 @@ class CreateLensNewFeedBottomSheetState
                 child: LinearGradientButton.primaryButton(
                   onTap: _isFormValid
                       ? () async {
-                          // TODO: Implement create feed logic
-                          // This should call your feed creation API
-                          AutoRouter.of(context).pop();
+                          print('create feed');
+                          print(_nameController.text);
+                          print(_descriptionController.text);
+                          print(_admins);
                         }
                       : null,
                   label: t.space.lens.createFeed,
@@ -147,6 +189,106 @@ class CreateLensNewFeedBottomSheetState
           ),
         ],
       ),
+    );
+  }
+}
+
+class _AdminField extends StatelessWidget {
+  final String value;
+  final Function(String) onChanged;
+  final VoidCallback onRemove;
+  final bool removable;
+
+  const _AdminField({
+    required this.value,
+    required this.onChanged,
+    required this.onRemove,
+    required this.removable,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final t = Translations.of(context);
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return IntrinsicHeight(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Expanded(
+            flex: 3,
+            child: LemonTextField(
+              initialText: value,
+              hintText: t.space.lens.adminInputHint,
+              onChange: onChanged,
+            ),
+          ),
+          SizedBox(width: Spacing.xSmall),
+          InkWell(
+            onTap: removable ? onRemove : null,
+            child: Container(
+              width: Sizing.large,
+              height: Sizing.large,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: removable ? Colors.transparent : colorScheme.outline,
+                ),
+                color:
+                    removable ? LemonColor.atomicBlack : colorScheme.background,
+                borderRadius: BorderRadius.circular(LemonRadius.large * 2),
+              ),
+              child: Center(
+                child: ThemeSvgIcon(
+                  color: colorScheme.onSecondary,
+                  builder: (filter) => Assets.icons.icClose.svg(
+                    width: Sizing.xSmall,
+                    height: Sizing.xSmall,
+                    colorFilter: filter,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _AddButton extends StatelessWidget {
+  final VoidCallback onPress;
+
+  const _AddButton({required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        InkWell(
+          onTap: onPress,
+          child: Container(
+            width: Sizing.xLarge,
+            height: Sizing.xLarge,
+            decoration: BoxDecoration(
+              color: colorScheme.secondaryContainer,
+              borderRadius: BorderRadius.circular(Sizing.xLarge),
+            ),
+            child: Center(
+              child: ThemeSvgIcon(
+                color: colorScheme.onSecondary,
+                builder: (filter) => Assets.icons.icAdd.svg(
+                  width: Sizing.xSmall,
+                  height: Sizing.xSmall,
+                  colorFilter: filter,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
