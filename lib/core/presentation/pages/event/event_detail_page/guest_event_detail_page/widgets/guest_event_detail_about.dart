b2ac7cf4994e377bfe2b1cedb9549355f449329b
Thinh CP <thinh@coderpush.com>
2025-05-14 21:25:35 +0700

    fix: fix bug navigate event in space, bug tap guest event description navigate to edit

--- lib/core/presentation/pages/event/event_detail_page/guest_event_detail_page/widgets/guest_event_detail_about.dart
+++ lib/core/presentation/pages/event/event_detail_page/guest_event_detail_page/widgets/guest_event_detail_about.dart
@@ -1,11 +1,14 @@
+import 'package:app/core/application/auth/auth_bloc.dart';
 import 'package:app/core/domain/event/entities/event.dart';
 import 'package:app/core/presentation/widgets/common/html_display/html_display.dart';
+import 'package:app/core/utils/event_utils.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/router/app_router.gr.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:app/theme/typo.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class GuestEventDetailAbout extends StatelessWidget {
   const GuestEventDetailAbout({
@@ -39,12 +42,19 @@ class GuestEventDetailAbout extends StatelessWidget {
         ],
         InkWell(
           onTap: () {
-            AutoRouter.of(context).push(
-              EventDescriptionFieldRoute(
-                description: event.description ?? '',
-                onSave: (value) async {},
-              ),
-            );
+            final userId = context.read<AuthBloc>().state.maybeWhen(
+                  authenticated: (authSession) => authSession.userId,
+                  orElse: () => '',
+                );
+            if (EventUtils.isOwnEvent(event: event, userId: userId) ||
+                EventUtils.isCohost(event: event, userId: userId)) {
+              AutoRouter.of(context).push(
+                EventDescriptionFieldRoute(
+                  description: event.description ?? '',
+                  onSave: (value) async {},
+                ),
+              );
+            }
           },
           child: HtmlDisplay(htmlContent: event.description ?? ''),
         ),
