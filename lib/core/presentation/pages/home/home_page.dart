f3be487aeb44c9bb796d4092cef8458303d47d9e
Sieu Thai <sieuhuflit@gmail.com>
2025-05-20 11:54:23 +0700

    feat: improve homescreen UI (#948) (#950)
    https://github.com/user-attachments/assets/db3dfac6-82b5-4cfa-8870-17dabbcd544f

--- lib/core/presentation/pages/home/home_page.dart
+++ lib/core/presentation/pages/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:app/app_theme/app_theme.dart';
 import 'package:app/core/presentation/pages/auth/login_page.dart';
 import 'package:app/core/presentation/pages/home/views/home_view.dart';
 import 'package:app/core/presentation/pages/home/widgets/quest_points_widget.dart';
@@ -8,7 +9,6 @@ import 'package:app/core/presentation/widgets/loading_widget.dart';
 import 'package:app/i18n/i18n.g.dart';
 import 'package:app/injection/register_module.dart';
 import 'package:app/router/app_router.gr.dart';
-import 'package:app/theme/color.dart';
 import 'package:app/theme/spacing.dart';
 import 'package:auto_route/auto_route.dart';
 
@@ -50,7 +50,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final t = Translations.of(context);
-    final colorScheme = Theme.of(context).colorScheme;
+    final appColors = Theme.of(context).appColors;
     final isLoggedIn = context.watch<AuthBloc>().state.maybeWhen(
           orElse: () => false,
           authenticated: (_) => true,
@@ -69,6 +69,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: HomeAppBar(
         title: "",
+        backgroundColor: appColors.pageBg,
         actions: [
           if (isProcessingLogin) ...[
             Loading.defaultLoading(context),
@@ -82,8 +83,8 @@ class _HomePageState extends State<HomePage> {
                   AutoRouter.of(context).navigate(LoginRoute());
                 },
                 label: t.auth.signIn,
-                backgroundColor: colorScheme.secondaryContainer,
-                borderColor: colorScheme.secondaryContainer,
+                backgroundColor: appColors.buttonSecondary,
+                borderColor: appColors.cardBorder,
                 radius: BorderRadius.circular(LemonRadius.button),
               ),
             ),
@@ -99,7 +100,7 @@ class _HomePageState extends State<HomePage> {
           ],
         ],
       ),
-      backgroundColor: LemonColor.black,
+      backgroundColor: appColors.pageBg,
       body: const HomeView(),
     );
   }
