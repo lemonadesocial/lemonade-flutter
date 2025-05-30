dfacc41bd26a5276b89bcd468209307412f67fcb
sieuhuflit <sieuhuflit@gmail.com>
2025-05-30 12:18:42 +0700

    feat: fully move to v2

--- lib/core/presentation/pages/edit_profile/edit_profile_page.dart
+++ lib/core/presentation/pages/edit_profile/edit_profile_page.dart
@@ -1,22 +1,9 @@
-import 'package:app/core/application/auth/auth_bloc.dart';
 import 'package:app/core/application/profile/edit_profile_bloc/edit_profile_bloc.dart';
 import 'package:app/core/presentation/pages/edit_profile/edit_profile_view_v2.dart';
-import 'package:app/core/presentation/pages/edit_profile/widgets/edit_profile_avatar.dart';
-import 'package:app/core/presentation/widgets/common/appbar/lemon_appbar_widget.dart';
-import 'package:app/core/presentation/widgets/theme_svg_icon_widget.dart';
-import 'package:app/core/utils/permission_utils.dart';
-import 'package:app/core/utils/snackbar_utils.dart';
-import 'package:app/gen/assets.gen.dart';
-import 'package:app/i18n/i18n.g.dart';
-import 'package:app/theme/color.dart';
-import 'package:app/theme/sizing.dart';
-import 'package:app/theme/spacing.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:app/app_theme/app_theme.dart';
 
 @RoutePage()
 class EditProfilePage extends StatelessWidget {
